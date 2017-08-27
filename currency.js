a64c91aa38f05b3dd36bbbd6496c10be45f94dc8
david stanton <davidstanton@spormeon.com>
2017-08-27 10:42:17 +0100

    currency files, so can try and pull currency each day, rather than have script in file pull them on the fly

---
+++ currency.js
@@ -0,0 +1,12 @@
+{
+   "dataAsOf":"2017-08-24",
+   "conversions":{
+      "USD":{        // from USD to other currencies
+         "GBP":0.77926,
+          [...]
+    },
+    "GBP": {         // can optionally supply direct conversions
+         [...]             // from multiple currencies
+    }
+  }
+}
