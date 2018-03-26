f1dadbecb0d92c2e9535d39687de7ed44c261a2a
david stanton <davidstanton@spormeon.com>
2018-03-26 12:40:18 +0100

    no message

--- prebid/Outstreamgolfwrx.js
+++ prebid/Outstreamgolfwrx.js
@@ -1,18 +1,5 @@
-(function () {
-        var gads = document.createElement('script');
-        gads.async = true;
-        gads.type = 'text/javascript';
-        var useSSL = 'https:' == document.location.protocol;
-        gads.src = (useSSL ? 'https:' : 'http:') +
-                '//www.googletagservices.com/tag/js/gpt.js';
-        var node = document.getElementsByTagName('script')[0];
-        node.parentNode.insertBefore(gads, node);
-    })();
-var googletag = googletag || {};
-  googletag.cmd = googletag.cmd || [];
-  googletag.cmd.push(function() {
-    googletag.defineSlot('/1001824/Outstream', [[300, 250], [728, 90], [1, 1]], 'div-gpt-ad-1522059993505-0').addService(googletag.pubads());
-    googletag.pubads().enableSingleRequest();
-    googletag.pubads().collapseEmptyDivs();
-    googletag.enableServices();
-  });
+!function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a);r.async=!0,r.src="https://www.googletagmanager.com/gtm.js?id=GTM-PL4PD49",m.parentNode.insertBefore(r,m)}(window.parent,window.parent.document,"script","dataLayer");
+var iframe=window.parent.document.createElement("iframe");iframe.src="https://www.googletagmanager.com/ns.html?id=GTM-PL4PD49";iframe.height="0";iframe.width="0";iframe.style="display:none;visibility:hidden";window.parent.document.body.appendChild(iframe);
+window.parent['$$mm-analytics'] = window.parent['$$mm-analytics'] || {};if (typeof window.parent['$$mm-analytics'].firstEmbed === 'undefined') {window.parent['$$mm-analytics'].firstEmbed = Date.now();}
+async type='text/javascript' src="https://bucket1.mm-syringe.com/prod/configs/616fa65d-129d-451b-96dd-551148aa9a44.js"
+async type='text/javascript' src="https://bucket1.mm-syringe.com/prod/injector/injector.1.1.js"
