f573a00495692e709397a2f7c5c6740bff6a2651
david stanton <davidstanton@spormeon.com>
2018-03-26 11:41:28 +0100

    no message

--- prebid/Outstreamgolfwrx.js
+++ prebid/Outstreamgolfwrx.js
@@ -1,21 +1,21 @@
 <script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
-<script>
+
   var googletag = googletag || {};
   googletag.cmd = googletag.cmd || [];
-</script>
 
-<script>
+
+
   googletag.cmd.push(function() {
     googletag.defineSlot('/1001824/Outstream', [[300, 250], [728, 90], [1, 1]], 'div-gpt-ad-1522059993505-0').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.pubads().collapseEmptyDivs();
     googletag.enableServices();
   });
-</script>
+
 
 <!-- /1001824/Outstream -->
 <div id='div-gpt-ad-1522059993505-0'>
-<script>
+
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1522059993505-0'); });
-</script>
+
 </div>
