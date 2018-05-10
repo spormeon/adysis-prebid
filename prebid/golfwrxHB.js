54ad861b47aaba81a9e2d0f767a0bfee5fd40847
david stanton <davidstanton@spormeon.com>
2018-05-10 12:01:32 +0100

    no message

--- prebid/golfwrxHB.js
+++ prebid/golfwrxHB.js
@@ -53,7 +53,7 @@ apstag.fetchBids({
      });
 }); 
   
-  var PREBID_TIMEOUT = 1500;
+  var PREBID_TIMEOUT = 900;
      
   var gpt_config = {
     prebid_timeout: 1000, //left for reference but not using right now...
