9dea4947c7d347fa6c669d300ba97bf9ed957317
david stanton <davidstanton@spormeon.com>
2018-06-02 11:25:55 +0100

    no message

--- prebid/golfwrxHB.js
+++ prebid/golfwrxHB.js
@@ -37,12 +37,14 @@ var phoneSizes = [
     [320, 50],
     [300, 250]
 ];
-var allSizes = [
+var 970Sizes = [
     [970, 250],
     [970, 90],
     [728,90],
     [468, 60],
-    [320, 50],
+    [320, 50]
+];
+var 360Sizes = [
     [300, 600],
     [300, 250],
     [160,600],
@@ -68,7 +70,7 @@ apstag.fetchBids({
      {
          slotID: 'topSlot', //example: 'div-gpt-ad-1475185990716-0'
          slotName: '1001824/prebid_test2', //example: '12345/leaderboard-1'
-         sizes: allSizes //example: [[728,90]]
+         sizes: 970Sizes //example: [[728,90]]
      },
      {
          slotID: 'middlerightSlot', //example: 'div-gpt-ad-1475185990716-0'
@@ -111,7 +113,7 @@ apstag.fetchBids({
   {
     code: 'bottomrightSlot',  // first right hand side 300x600 
     
-    mediaTypes: { banner: { sizes: allSizes } },
+    mediaTypes: { banner: { sizes: 360Sizes } },
     
     bids: [
       { bidder: 'aol',          params: { placement: '4882886', network: '4436.1', server: 'adserver.adtech.de' } }, /* 300x250 */ 
@@ -158,7 +160,7 @@ apstag.fetchBids({
   {
     code: 'middlerightSlot', //second right hand side 300x600
     
-    mediaTypes: { banner: { sizes: allSizes } },
+    mediaTypes: { banner: { sizes: 360Sizes } },
     
     bids: [
       { bidder: 'aol',          params: { placement: '4882886', network: '4436.1', server: 'adserver.adtech.de' } }, /* 300x250 */ 
@@ -205,7 +207,7 @@ apstag.fetchBids({
   {
     code: 'bottomleftSlot',  // first left hand side 300x600
     
-    mediaTypes: { banner: { sizes: allSizes } },
+    mediaTypes: { banner: { sizes: 360Sizes } },
     
     bids: [
       { bidder: 'aol',          params: { placement: '4882886', network: '4436.1', server: 'adserver.adtech.de' } }, /* 300x250 */ 
@@ -252,7 +254,7 @@ apstag.fetchBids({
   {
     code: 'topSlot',
     
-    mediaTypes: { banner: { sizes: allSizes } },
+    mediaTypes: { banner: { sizes: 970Sizes } },
     
     bids: [
       { bidder: 'aol',          params: { placement: '4882887', network: '4436.1', server: 'adserver.adtech.de' } }, /* 728x90 */
@@ -422,10 +424,10 @@ apstag.fetchBids({
   
   
   googletag.cmd.push(function() {
-      topSlot = googletag.defineSlot('/1001824/prebid_test2', allSizes, 'topSlot').setTargeting("test", "refresh").addService(googletag.pubads());
-      middlerightSlot = googletag.defineSlot('/1001824/prebid_test3', allSizes, 'middlerightSlot').setTargeting("test", "refresh").addService(googletag.pubads());
-      bottomrightSlot = googletag.defineSlot('/1001824/prebid_test1', allSizes, 'bottomrightSlot').setTargeting("test", "refresh").addService(googletag.pubads());
-      bottomleftSlot = googletag.defineSlot('/1001824/prebid_test4', allSizes, 'bottomleftSlot').setTargeting("test", "refresh").addService(googletag.pubads());
+      topSlot = googletag.defineSlot('/1001824/prebid_test2', 970Sizes, 'topSlot').setTargeting("test", "refresh").addService(googletag.pubads());
+      middlerightSlot = googletag.defineSlot('/1001824/prebid_test3', 360Sizes, 'middlerightSlot').setTargeting("test", "refresh").addService(googletag.pubads());
+      bottomrightSlot = googletag.defineSlot('/1001824/prebid_test1', 360Sizes, 'bottomrightSlot').setTargeting("test", "refresh").addService(googletag.pubads());
+      bottomleftSlot = googletag.defineSlot('/1001824/prebid_test4', 360Sizes, 'bottomleftSlot').setTargeting("test", "refresh").addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.enableServices();
     googletag.display("topSlot");
