c64c2a05b9ba0ba7ae96979e4a55fc4ab60b02bc
david stanton <davidstanton@spormeon.com>
2018-06-02 11:57:37 +0100

    no message

--- prebid/golfwrxHB.js
+++ prebid/golfwrxHB.js
@@ -14,7 +14,6 @@ window.snowplow('trackPageView');
 
 <!-- Prebid Config Section START -->
 <!-- Make sure this is inserted before your GPT tag -->
-
 //load the apstag.js library
 !function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
 
