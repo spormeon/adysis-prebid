9d5374ac560f3f6379ef40071b820ff711a5f39e
david stanton <davidstanton@spormeon.com>
2018-06-08 12:13:30 +0100

    no message

--- prebid/golfwrxinviewHB.js
+++ prebid/golfwrxinviewHB.js
@@ -52,7 +52,7 @@ apstag.fetchBids({
      });
 }); 
   
-  var PREBID_TIMEOUT = 700;
+  var PREBID_TIMEOUT = 600;
      
   var gpt_config = {
     prebid_timeout: 1000, //left for reference but not using right now...
@@ -316,7 +316,7 @@ apstag.fetchBids({
               allowAuctionWithoutConsent: true
             },
     	  publisherDomain: "golfwrx.com",
-    	  bidderTimeout: 1500,
+    	  bidderTimeout: 1200,
     	  "currency": {
    	       "adServerCurrency": "GBP",
    	       "granularityMultiplier": 1
