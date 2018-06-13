e586e9e85654ac5f88b5ebde1706f7f966a92e0d
david stanton <davidstanton@spormeon.com>
2018-06-13 16:55:57 +0100

    no message

--- prebid/golfwrxinviewHB.js
+++ prebid/golfwrxinviewHB.js
@@ -283,7 +283,7 @@ apstag.fetchBids({
       var pbjsEl = document.createElement("script");
       pbjsEl.type = "text/javascript";
       pbjsEl.async = true;
-      pbjsEl.src = "//d3s34vlfe7g7ew.cloudfront.net/allAdapters.js";
+      pbjsEl.src = "https://acdn.adnxs.com/prebid/not-for-prod/1/prebid.js";
       var pbjsTargetEl = document.getElementsByTagName("head")[0];
       pbjsTargetEl.insertBefore(pbjsEl, pbjsTargetEl.firstChild);
   })();
