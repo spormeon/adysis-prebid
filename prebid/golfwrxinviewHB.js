622135ce9d142cfc83555b83121c2fd7273aa84a
david stanton <davidstanton@spormeon.com>
2018-06-19 10:27:22 +0100

    no message

--- prebid/golfwrxinviewHB.js
+++ prebid/golfwrxinviewHB.js
@@ -257,13 +257,7 @@
   },
   {
 	    code: 'skinSlot',
-	    mediaTypes: {
-	        banner: {
-	            sizes: [
-	            	[1, 1]
-	            ],
-	        }
-	    },   
+	    sizes: [[1, 1]], 
 	    bids: [
 	      { bidder: 'justpremium',    labelAny: ['desktop', 'tablet'],      params: { zone: '50978', allow: ['wp', 'sa', 'pu', 'pd', 'pa', 'ms', 'mo', 'is', 'as', 'fa', 'fi', 'hi', 'cf']} } /* wallpaper, video wallpaper */
 	    ] //./bids
