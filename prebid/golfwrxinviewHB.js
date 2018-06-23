86296565acf2e57763b7a985fa62986fd6e4e1cb
david stanton <davidstanton@spormeon.com>
2018-06-23 11:44:48 +0100

    trying to move analytics to own file in case code changes later

--- prebid/golfwrxinviewHB.js
+++ prebid/golfwrxinviewHB.js
@@ -1,32 +1,6 @@
 var PREBID_TIMEOUT = 1800;
 
-
-// ASSERTIVE ANALYTICS SETTINGS - Version: 1.4.0
-var assertive_entityId = 'eE88aE2HtwKnXikcL';
-var assertive_debug = 0; // append the query string 'assertiveYield=debug' or add this local storage entry 'localStorage.setItem("assertiveYield", "debug")' to enable dynamically
-var assertive_sampleRate = 1; // 1 = 100%, 0.2 = 20%...
-
-var assertive_timeout = null;
-var assertive_layout = null;
-var assertive_userState = null;
-
-// example of supplying a custom var from a prev. defined var
-assertive_timeout = PREBID_TIMEOUT;
-
-// example of reading data from website and supplying as custom var
-var DOMReady = function(a,b,c){b=document,c='addEventListener';b[c]?b[c]('DOMContentLoaded',a):window.attachEvent('onload',a);};
-DOMReady(function () {
-    try {
-        var innerHTML = document.querySelector('body > div.navbar ul.nav > li:last-child > a').innerHTML;
-        if ( innerHTML === "Logout" ) {
-            assertive_userState = 'Anonymous';
-        } else if ( innerHTML === "Register" ) {
-            assertive_userState = 'LoggedIn';
-        }
-    } catch(e) {
-        console.log(e);
-    }
-});  
+document.write('<scr'+'ipt type="text/javascript" src="//d3s34vlfe7g7ew.cloudfront.net/prebidanalytics.js" ></scr'+'ipt>'); 
 
 //load up prebid.js,  I think we need to load this earlier
   (function() {
