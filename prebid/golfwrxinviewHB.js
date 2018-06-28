0c118d9c02f5f378f65e41a47d41a1564dea899a
david stanton <davidstanton@spormeon.com>
2018-06-28 13:30:05 +0100

    no message

--- prebid/golfwrxinviewHB.js
+++ prebid/golfwrxinviewHB.js
@@ -507,7 +507,7 @@ DOMReady(function () {
     	    Object.keys(config.definitons).forEach(function (key) {
     	      var def = config.definitons[key];
     	      var slot = googletag.defineSlot(def.adUnitPath, def.size, key);
-    	      
+    	      slot.setTargeting();
     	      slot.defineSizeMapping(sizeMappings[def.sizeMapping]);
     	      slot.addService(googletag.pubads());
     	      googletag.display(key);
