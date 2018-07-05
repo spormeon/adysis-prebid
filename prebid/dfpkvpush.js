28ae8e2cfac54f8a13f10acc44f844cd4e1d5da6
david stanton <davidstanton@spormeon.com>
2018-07-05 12:58:59 +0100

    no message

--- prebid/dfpkvpush.js
+++ prebid/dfpkvpush.js
@@ -16,10 +16,18 @@ var googletag = googletag || {};
    googletag.defineSlot("/1001824/prebid_test2", [[970, 250],[970, 90],[728, 90],[468, 60],[320, 50],[234, 60]], "topSlot")
     .addService(googletag.pubads())
      .setTargeting("hb_pb", "0.50");
-   googletag.defineSlot("/1001824/prebid_test3", [[300, 600],[300, 250],[160, 600],[120, 600],[250, 250]], "middlerightSlot")
+   googletag.defineSlot("/1001824/prebid_test3", [[300, 600],[300, 250],[160, 600],[120, 600],[250, 250]], "toprightSlot")
      .addService(googletag.pubads())
      .setTargeting("hb_pb", "0.50")
      .setTargeting("age", "20-30");
+   googletag.defineSlot("/1001824/prebid_test1", [[300, 600],[300, 250],[160, 600],[120, 600],[250, 250]], "middlerightSlot")
+   .addService(googletag.pubads())
+   .setTargeting("hb_pb", "0.50")
+   .setTargeting("age", "20-30");
+   googletag.defineSlot("/1001824/prebid_test4", [[300, 600],[300, 250],[160, 600],[120, 600],[250, 250]], "bottomrightSlot")
+   .addService(googletag.pubads())
+   .setTargeting("hb_pb", "0.50")
+   .setTargeting("age", "20-30");
    googletag.pubads().setTargeting("topic","basketball");
    googletag.pubads().enableSingleRequest();
    googletag.enableServices();
