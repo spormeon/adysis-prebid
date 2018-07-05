35dfa8ff285071fb514ec7276263faa3450e9b02
david stanton <davidstanton@spormeon.com>
2018-07-05 10:44:49 +0100

    no message

--- prebid/golfwrxinviewHB.js
+++ prebid/golfwrxinviewHB.js
@@ -356,12 +356,12 @@ DOMReady(function () {
     		  "buckets" : [{
     		      "precision": 2,  //default is 2 if omitted - means 2.1234 rounded to 2 decimal places = 2.12
     		      "min" : 0,
-    		      "max" : 1.1,
+    		      "max" : 1.5,
     		      "increment" : 0.01  // from $0 to $5, 1-cent increments
     		    },
     		    {
     		      "precision": 2,
-    		      "min" : 1.2,
+    		      "min" : 1.6,
     		      "max" : 20,
     		      "increment" : 0.10  // from $5 to $20, round down to the previous 10-cent increment
     		    }]
