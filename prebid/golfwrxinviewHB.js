80aac53364131666b446ead5678bf05ab3c59208
david stanton <davidstanton@spormeon.com>
2018-07-18 10:46:25 +0100

    no message

--- prebid/golfwrxinviewHB.js
+++ prebid/golfwrxinviewHB.js
@@ -109,10 +109,10 @@ DOMReady(function () {
       /* { bidder: 'springserveAlias2',  params: { placementId: '12463797'} }  */ /* 300x600 - 300x250 - 160x600 - 120x600 */
       { bidder: 'adsparc',      labelAny: ['desktop', 'tablet', 'phone'], params: { networkId: '9969', siteId: '1019801'} }, /*one placementId for all sizes*/
       { bidder: 'contentignite',      labelAny: ['desktop', 'tablet', 'phone'], params: { accountID: '168237', zoneID: '316288'} }, /*300x250*/
-      { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '53864'} }, /*300x250*/
-      { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53866'} }, /*300x600*/
-      { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53863'} }, /*160x600*/
-      { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '54662'} }, /*120x600*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '53864'} }, */ /*300x250*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53866'} }, */ /*300x600*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53863'} }, */ /*160x600*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '54662'} }, */ /*120x600*/
       { bidder: 'gumgum',      labelAny: ['desktop'], params: { inSlot: '14598' } }, /*gumgum-160x600*/
       { bidder: 'gumgum',      labelAny: ['desktop', 'tablet', 'phone'], params: { inSlot: '14600' } }, /*gumgum-300x250*/
       { bidder: 'gumgum',      labelAny: ['desktop'], params: { inSlot: '14601' } }, /*gumgum-300x600*/
@@ -175,10 +175,10 @@ DOMReady(function () {
       /* { bidder: 'springserveAlias2',  params: { placementId: '12463797'} }  */ /* 300x600 - 300x250 - 160x600 - 120x600 */
       { bidder: 'adsparc',      labelAny: ['desktop', 'tablet', 'phone'], params: { networkId: '9969', siteId: '1019801'} }, /*one placementId for all sizes*/
       { bidder: 'contentignite',      labelAny: ['desktop', 'tablet', 'phone'], params: { accountID: '168237', zoneID: '316288'} }, /*300x250*/
-      { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '53864'} }, /*300x250*/
-      { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53866'} }, /*300x600*/
-      { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53863'} }, /*160x600*/
-      { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '54662'} }, /*120x600*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '53864'} }, */ /*300x250*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53866'} }, */ /*300x600*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53863'} }, */ /*160x600*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '54662'} }, */ /*120x600*/
       { bidder: 'gumgum',      labelAny: ['desktop'], params: { inSlot: '14598' } }, /*gumgum-160x600*/
       { bidder: 'gumgum',      labelAny: ['desktop', 'tablet', 'phone'], params: { inSlot: '14600' } }, /*gumgum-300x250*/
       { bidder: 'gumgum',      labelAny: ['desktop'], params: { inSlot: '14601' } }, /*gumgum-300x600*/
@@ -241,10 +241,10 @@ DOMReady(function () {
       /* { bidder: 'springserveAlias2',  params: { placementId: '12463797'} }  */ /* 300x600 - 300x250 - 160x600 - 120x600 */
       { bidder: 'adsparc',      labelAny: ['desktop', 'tablet', 'phone'], params: { networkId: '9969', siteId: '1019801'} }, /*one placementId for all sizes*/
       { bidder: 'contentignite',      labelAny: ['desktop', 'tablet', 'phone'], params: { accountID: '168237', zoneID: '316288'} }, /*300x250*/
-      { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '53864'} }, /*300x250*/
-      { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53866'} }, /*300x600*/
-      { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53863'} }, /*160x600*/
-      { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '54662'} }, /*120x600*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '53864'} }, */ /*300x250*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53866'} }, */ /*300x600*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '53863'} }, */ /*160x600*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop'], params: { zoneid: '54662'} }, */ /*120x600*/
       { bidder: 'gumgum',      labelAny: ['desktop'], params: { inSlot: '14598' } }, /*gumgum-160x600*/
       { bidder: 'gumgum',      labelAny: ['desktop', 'tablet', 'phone'], params: { inSlot: '14600' } }, /*gumgum-300x250*/
       { bidder: 'gumgum',      labelAny: ['desktop'], params: { inSlot: '14601' } }, /*gumgum-300x600*/
@@ -299,9 +299,9 @@ DOMReady(function () {
       { bidder: 'adsparc',      labelAny: ['desktop', 'tablet', 'phone'], params: { networkId: '9969', siteId: '1019801'} }, /*one placementId for all sizes*/
       { bidder: 'contentignite',      labelAny: ['desktop', 'tablet'], params: { accountID: '168237', zoneID: '316287'} }, /*728x90*/
       { bidder: 'contentignite',      labelAny: ['desktop', 'tablet', 'phone'], params: { accountID: '168237', zoneID: '316289'} }, /*320x50*/
-      { bidder: 'fidelity',      labelAny: ['desktop', 'tablet'], params: { zoneid: '53862'} }, /*728x90*/
-      { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '53865'} }, /*320x50*/
-      { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '54663'} }, /*468x60*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop', 'tablet'], params: { zoneid: '53862'} }, */ /*728x90*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '53865'} }, */ /*320x50*/
+      /* { bidder: 'fidelity',      labelAny: ['desktop', 'tablet', 'phone'], params: { zoneid: '54663'} }, */ /*468x60*/
       { bidder: 'gumgum',      labelAny: ['desktop'], params: { inSlot: '14605' } }, /*gumgum-970x90*/
       { bidder: 'gumgum',      labelAny: ['desktop', 'tablet'], params: { inSlot: '14604' } }, /*gumgum-728x90*/
       { bidder: 'gumgum',      labelAny: ['desktop', 'tablet', 'phone'], params: { inSlot: '14603' } }, /*gumgum-468x60*/
