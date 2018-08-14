eb9ab3faa352a42cbfc7e33a9e67dad9c4e072a0
david stanton <davidstanton@spormeon.com>
2018-08-14 12:19:38 +0100

    no message

--- prebid/golfwrxinviewHB.js
+++ prebid/golfwrxinviewHB.js
@@ -443,20 +443,7 @@ var googletag = googletag || {};
 googletag.cmd = googletag.cmd || [];
 
 
-const customConfigObject = {
-		  "buckets" : [{
-		      "precision": 2,  //default is 2 if omitted - means 2.1234 rounded to 2 decimal places = 2.12
-		      "min" : 0,
-		      "max" : 2,
-		      "increment" : 0.01  // from $0 to $5, 1-cent increments
-		    },
-		    {
-		      "precision": 2,
-		      "min" : 2.1,
-		      "max" : 20,
-		      "increment" : 0.10  // from $5 to $20, round down to the previous 10-cent increment
-		    }]
-		};
+
 
 var pbjs = pbjs || {};
 pbjs.que = pbjs.que || [];
@@ -471,6 +458,21 @@ pbjs.bidderSettings = {
 pbjs.aliasBidder('appnexus','brealtime');  // alias for bidder
 pbjs.aliasBidder('appnexus','springserveAlias2'); // alias for bidder
 pbjs.aliasBidder('appnexus','districtm'); // alias for bidder
+
+const customConfigObject = {
+		  "buckets" : [{
+		      "precision": 2,  //default is 2 if omitted - means 2.1234 rounded to 2 decimal places = 2.12
+		      "min" : 0,
+		      "max" : 2,
+		      "increment" : 0.01  // from $0 to $5, 1-cent increments
+		    },
+		    {
+		      "precision": 2,
+		      "min" : 2.1,
+		      "max" : 20,
+		      "increment" : 0.10  // from $5 to $20, round down to the previous 10-cent increment
+		    }]
+		};
 
 pbjs.setConfig({
 	  priceGranularity: customConfigObject,
