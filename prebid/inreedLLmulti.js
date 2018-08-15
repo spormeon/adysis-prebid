e02092af3d7e58f703cccf0834d2a5044d68f2bd
david stanton <davidstanton@spormeon.com>
2018-08-15 21:18:37 +0100

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -18,7 +18,19 @@
           '//www.googletagservices.com/tag/js/gpt.js';
   var node = document.getElementsByTagName('script')[0];
   node.parentNode.insertBefore(gads, node);
-})();        
+})(); 
+
+//load up google gpt.js
+(function () {
+  var gads = document.createElement('script');
+  gads.async = true;
+  gads.type = 'text/javascript';
+  var useSSL = 'https:' == document.location.protocol;
+  gads.src = (useSSL ? 'https:' : 'http:') +
+          '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js';
+  var node = document.getElementsByTagName('script')[0];
+  node.parentNode.insertBefore(gads, node);
+})();  
          
 var timeoutMap = {
 		   0 : 1500,
