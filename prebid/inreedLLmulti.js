2c793f972bf52a03099e7cbd2ed3440b0f54365d
david stanton <davidstanton@spormeon.com>
2018-08-16 15:55:46 +0100

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -88,7 +88,7 @@ var timeoutMap = {
             	 code: 'inreedvidSlot',
             	 mediaTypes: {
             		 banner: { sizes: [[728, 90], [300, 250], [320, 50], [1, 1]] },
-            	     video:  { context: 'outstream', playerSize: [640, 480] }, 
+            	     video:  { context: 'outstream', playerSize: [640, 480] } 
             	 },
             	 renderer: {
             	        url: 'http://cdn.adnxs.com/renderer/video/ANOutstreamVideo.js',
