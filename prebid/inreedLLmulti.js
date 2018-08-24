913b86888c3d182133872fb887ec3265873e6204
david stanton <davidstanton@spormeon.com>
2018-08-24 14:10:04 +0100

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -320,7 +320,7 @@ googletag.display('inreedvid3Slot');
 });
 // have to add 1 on to the child (number) because the div counts as 1 in the string
 $(document).ready(function(){
-	  $('<div class="advertisement">Advertisement</div><div class="ad-reporter-ahytrfg35423"><div id="inreedvidSlot"></div></div>').insertAfter('#mvp-main-box p:nth-child(3) ');
+	  $('<div class="advertisement">Advertisement</div><div class="ad-reporter-ahytrfg35423"><div id="inreedvidSlot" style="width:550px;height:310px ></div></div>').insertAfter('#mvp-main-box p:nth-child(3) ');
 	  $('<div class="advertisement">Advertisement</div><div class="ad-reporter-ahytrfg35423"><div id="inreedvid1Slot"></div></div>').insertAfter('#mvp-main-box p:nth-child(20) ');
 	  $('<div class="advertisement">Advertisement</div><div class="ad-reporter-ahytrfg35423"><div id="inreedvid2Slot"></div></div>').insertAfter('#mvp-main-box p:nth-child(40) ');
 	  $('<div class="advertisement">Advertisement</div><div class="ad-reporter-ahytrfg35423"><div id="inreedvid3Slot"></div></div>').insertAfter('#mvp-main-box p:nth-child(60) ');
