b39bab5076fa404b3a238ccd40cf7fc7ec1d9ff0
david stanton <davidstanton@spormeon.com>
2018-08-28 17:18:33 +0100

    no message

--- prebid/golfopinions.js
+++ prebid/golfopinions.js
@@ -302,7 +302,7 @@ googletag.pubads().enableLazyLoad({
     	renderMarginPercent: 40,  // Render slots within 5000 viewports.
     	mobileScaling: 1.0  // Double the above values on mobile.
 });
-googletag.pubads().enableSingleRequest();
+// googletag.pubads().enableSingleRequest();
 googletag.pubads().collapseEmptyDivs(true, true);
 googletag.pubads().setCentering(true);
 googletag.pubads().enableVideoAds();
