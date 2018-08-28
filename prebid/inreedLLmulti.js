5a361926cf67e85719d9dffc0c3b5ec14cc88656
david stanton <davidstanton@spormeon.com>
2018-08-28 11:18:20 +0100

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -231,7 +231,7 @@ var timeoutMap = {
                  s2sConfig: {
                      accountId: 'e31f627f-53a3-4288-9979-482d3c6ffc76',
                      enabled: true,
-                     bidders: ['somoaudience', 'conversant', 'pulsepoint', 'appnexus'],
+                     bidders: ['somoaudience', 'conversant', 'pulsepoint', 'appnexus', 'districtm'],
                      timeout: 1000,
                      adapter: 'prebidServer',
                      endpoint: 'https://prebid.adnxs.com/pbs/v1/openrtb2/auction',
