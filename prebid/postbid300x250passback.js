3d5884fb0a3e84f9a9771ba855f55bfbbd0c6997
david stanton <davidstanton@spormeon.com>
2018-09-20 13:02:03 +0100

    no message

---
+++ prebid/postbid300x250passback.js
@@ -0,0 +1,3 @@
+<div id="switch_placeholder_33bfdf60b4dfc96eda0b9d4bac34f5d9" class="switch_placeholder"></div>
+<script> (__scads = window.__scads || []).push({"z":8608,"targetId":"switch_placeholder_33bfdf60b4dfc96eda0b9d4bac34f5d9","domain":"delivery.c.switchadhub.com","width":"0","height":"0"}); </script>
+<script async src="//delivery.c.switchadhub.com/adserver/sat.js"></script>
