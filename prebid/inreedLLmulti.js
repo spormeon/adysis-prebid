a3ba8b6312dfc36ecb1f7eacd4d1aeea2c3f759c
david stanton <davidstanton@spormeon.com>
2018-11-15 14:21:54 +0000

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -293,9 +293,9 @@ $(document).ready(function(){
 // div-2 is 3 viewports down, div-3 is 12 viewports down.
 googletag.cmd.push(function() {
     	  inreedvidSlot = googletag.defineSlot('/1001824/Golfwrx.com-HB/Golfwrx.com-HB-Vid-test',  [[300,250],[250,250],[728,90],[468,60],[320,50],[1,1],[640,480]], 'inreedvidSlot').addService(googletag.pubads());
-    	  inreedvid1Slot = googletag.defineSlot('/1001824/Golfwrx.com-HB/Golfwrx.comHB-Vid-test1', [[300,250],[728,90],[1,1],[640, 480]], 'inreedvid1Slot').addService(googletag.pubads());
-    	  inreedvid2Slot = googletag.defineSlot('/1001824/Golfwrx.com-HB/Golfwrx.comHB-Vid-test2', [[300,250],[728,90],[1,1]], 'inreedvid2Slot').addService(googletag.pubads());
-    	  inreedvid3Slot = googletag.defineSlot('/1001824/Golfwrx.com-HB/Golfwrx.comHB-Vid-test3', [[300,250],[728,90],[1,1]], 'inreedvid3Slot').addService(googletag.pubads());
+    	  inreedvid1Slot = googletag.defineSlot('/1001824/Golfwrx.com-HB/Golfwrx.comHB-Vid-test1', [[300,250],[250,250],[728,90],[468,60],[320,50],[1,1],[640,480]], 'inreedvid1Slot').addService(googletag.pubads());
+    	  inreedvid2Slot = googletag.defineSlot('/1001824/Golfwrx.com-HB/Golfwrx.comHB-Vid-test2', [[300,250],[250,250],[728,90],[468,60],[320,50],[1,1],[640,480]], 'inreedvid2Slot').addService(googletag.pubads());
+    	  inreedvid3Slot = googletag.defineSlot('/1001824/Golfwrx.com-HB/Golfwrx.comHB-Vid-test3', [[300,250],[250,250],[728,90],[468,60],[320,50],[1,1],[640,480]], 'inreedvid3Slot').addService(googletag.pubads());
 //googletag.pubads().setForceSafeFrame(true);
 //  var pageConfig = {
 //    allowOverlayExpansion: true,
