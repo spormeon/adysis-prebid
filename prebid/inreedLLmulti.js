6ccb4fbf4468f37a3ffb22fb57930e75dde238a3
david stanton <davidstanton@spormeon.com>
2019-03-11 12:26:02 +0000

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -1,6 +1,6 @@
 function dynamicallyLoadScript(url) {
     var script = document.createElement("script");  // create a script DOM node
-    script.src = "//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";  // set its src to the provided URL
+    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";  // set its src to the provided URL
 
     document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 }
