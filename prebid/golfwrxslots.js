9446b7103b05904489c73f957f30589816ba9175
david stanton <davidstanton@spormeon.com>
2019-03-12 12:30:53 +0000

    no message

--- prebid/golfwrxslots.js
+++ prebid/golfwrxslots.js
@@ -1,4 +1,4 @@
-window.addEventListener('onload',function(){
+window.addEventListener('load',function(){
     document.querySelector('#mvp-content-main p:nth-child(2)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvidSlot"></div></div><br>');
     document.querySelector('#mvp-content-main p:nth-child(6)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
     document.querySelector('#mvp-content-main p:nth-child(10)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
