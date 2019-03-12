0c5bdea253df5959e3e4acd01ea6d1c99b053383
david stanton <davidstanton@spormeon.com>
2019-03-12 10:49:13 +0000

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -23,9 +23,9 @@
 //have to add 1 on to the child (number) because the div counts as 1 in the string
 window.addEventListener('load',function(){
           document.querySelector('#mvp-content-main p:nth-child(2)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvidSlot"></div></div><br>');
-          document.querySelector('#mvp-content-main p:nth-child(4)').insertAdjacentHTML('afterbegin','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
-          document.querySelector('#mvp-content-main p:nth-child(6)').insertAdjacentHTML('afterbegin','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
-          document.querySelector('#mvp-content-main p:nth-child(8)').insertAdjacentHTML('afterbegin','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid3Slot"></div></div><br>');
+          document.querySelector('#mvp-content-main p:nth-child(6)').insertAdjacentHTML('afterbegin','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
+          document.querySelector('#mvp-content-main p:nth-child(10)').insertAdjacentHTML('afterbegin','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
+          document.querySelector('#mvp-content-main p:nth-child(14)').insertAdjacentHTML('afterbegin','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid3Slot"></div></div><br>');
 })
 
 //  1000 def brings back bigger bids in Uk
