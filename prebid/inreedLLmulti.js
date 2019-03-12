a564dfb610c44e16007eee83b1dc2204cd4c5a9c
david stanton <davidstanton@spormeon.com>
2019-03-12 11:59:30 +0000

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -22,11 +22,11 @@
     pbjsTargetEl.insertBefore(pbjsEl, pbjsTargetEl.firstChild);
 })();
  
-window.addEventListener('load',function(){
-    document.querySelector('#mvp-content-main p:nth-child(2)').insertAdjacentHTML('beforeend','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvidSlot"></div></div><br>');
-    document.querySelector('#mvp-content-main p:nth-child(6)').insertAdjacentHTML('beforeend','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
-    document.querySelector('#mvp-content-main p:nth-child(10)').insertAdjacentHTML('beforeend','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
-    document.querySelector('#mvp-content-main p:nth-child(14)').insertAdjacentHTML('beforeend','<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid3Slot"></div></div><br>');
+window.open.addEventListener('load',function(){
+    document.querySelector('#mvp-content-main p:nth-child(2)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvidSlot"></div></div><br>');
+    document.querySelector('#mvp-content-main p:nth-child(6)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
+    document.querySelector('#mvp-content-main p:nth-child(10)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
+    document.querySelector('#mvp-content-main p:nth-child(14)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid3Slot"></div></div><br>');
 })
 
 //  1000 def brings back bigger bids in Uk
