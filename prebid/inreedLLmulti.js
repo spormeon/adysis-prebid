c2377b34052ef038f974ae3ff6e444dc361cda28
david stanton <davidstanton@spormeon.com>
2019-03-12 09:37:13 +0000

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -20,13 +20,7 @@
 })();
 
 
-//have to add 1 on to the child (number) because the div counts as 1 in the string
-  $(document).ready(function(){
-     $('<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvidSlot"></div></div><br>').insertAfter('#mvp-content-main p:nth-child(2)');
-     $('<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid1Slot"></div></div><br>').insertAfter('#mvp-content-main p:nth-child(6)');
-     $('<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>').insertAfter('#mvp-content-main p:nth-child(10)');
-     $('<div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid3Slot"></div></div><br>').insertAfter('#mvp-content-main p:nth-child(18)');
-  });
+
   
   window.addEventListener('load',function(){
 	    document.querySelector('#mvp-content-main p:nth-child(1)').insertAdjacentHTML('afterend','<div class="ad-reporter-ahytrfg35423">Advertisement<div id="inreedvidSlot"></div><div>');
