f0b769d939863d265f6898b84dd24f286e3549cd
david stanton <davidstanton@spormeon.com>
2019-03-14 18:06:37 +0000

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -1,4 +1,10 @@
-
+//have to add 1 on to the child (number) because the div counts as 1 in the string 
+window.addEventListener('load',function(){
+    document.querySelector('#mvp-content-main p:nth-child(2)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvidSlot"></div></div><br>');
+    document.querySelector('#mvp-content-main p:nth-child(6)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
+    document.querySelector('#mvp-content-main p:nth-child(10)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
+    document.querySelector('#mvp-content-main p:nth-child(14)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid3Slot"></div></div><br>');
+})
 
 //load up google gpt.js
 (function () {
@@ -309,13 +315,7 @@ googletag.cmd.push(function() {
 // };
 // googletag.pubads().setSafeFrameConfig(pageConfig);
 // googletag.pubads().setSafeFrameConfig({sandbox: true});
-    	//have to add 1 on to the child (number) because the div counts as 1 in the string 
-    	  window.addEventListener('load',function(){
-    	      document.querySelector('#mvp-content-main p:nth-child(2)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvidSlot"></div></div><br>');
-    	      document.querySelector('#mvp-content-main p:nth-child(6)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
-    	      document.querySelector('#mvp-content-main p:nth-child(10)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
-    	      document.querySelector('#mvp-content-main p:nth-child(14)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid3Slot"></div></div><br>');
-    	  })
+
    	  
 googletag.pubads().enableLazyLoad({
     	fetchMarginPercent: 25,  // Fetch slots within 30 viewports.
