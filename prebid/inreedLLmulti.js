393d603aaf5789c581b0ca560500d78d70ba3ebc
david stanton <davidstanton@spormeon.com>
2019-03-14 22:49:37 +0000

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -12,7 +12,7 @@
   node.parentNode.insertBefore(gads, node);
 })(); 
 //have to add 1 on to the child (number) because the div counts as 1 in the string 
-window.addEventListener('onload',function(){
+window.addEventListener('load',function(){
     document.querySelector('#mvp-content-main p:nth-child(2)').insertAdjacentHTML('afterbegin','<div id="inreedvidSlot"></div>');
     document.querySelector('#mvp-content-main p:nth-child(6)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
     document.querySelector('#mvp-content-main p:nth-child(10)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div class="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
