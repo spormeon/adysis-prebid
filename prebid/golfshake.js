77c5920b26e7273b896b652c2e4c8dd2ae63bdc0
david stanton <davidstanton@spormeon.com>
2019-03-15 15:06:41 +0000

    no message

--- prebid/golfshake.js
+++ prebid/golfshake.js
@@ -1,4 +1,4 @@
-document.getElementById("advertisement").style.color = "blue";
+
 //have to add 1 on to the child (number) because the div counts as 1 in the string 
 window.addEventListener('DOMContentLoaded',function(){
     document.querySelector('.richcontent p:nth-child(2)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement">Advertisement</div><div id="inreedvidSlot"></div></div><br>');
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded',function(){
     document.querySelector('.richcontent p:nth-child(10)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
     document.querySelector('.richcontent p:nth-child(14)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement">Advertisement</div><div id="inreedvid3Slot"></div></div><br>');
 });
-
+document.getElementById("advertisement").style.color = "blue";
 //load up google gpt.js
 (function () {
   var gads = document.createElement('script');
