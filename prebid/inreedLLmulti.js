b5707fe028a086a1d5d7fa2cdd94b7faf2258617
david stanton <davidstanton@spormeon.com>
2019-03-15 11:22:14 +0000

    no message

--- prebid/inreedLLmulti.js
+++ prebid/inreedLLmulti.js
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded',function(){
     var pbjsEl = document.createElement("script");
     pbjsEl.type = "text/javascript";
     pbjsEl.async = true;
-    pbjsEl.src = "//d3s34vlfe7g7ew.cloudfront.net/prebid2.5.1.js";
+    pbjsEl.src = "//d3s34vlfe7g7ew.cloudfront.net/prebid2.6.0.js";
     var pbjsTargetEl = document.getElementsByTagName("footerbid")[0];
     pbjsTargetEl.insertBefore(pbjsEl, pbjsTargetEl.firstChild);
 })();
