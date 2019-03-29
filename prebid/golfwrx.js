6d55ae3c9c2bf8217741a41bb76cc4391687cfa8
david stanton <davidstanton@spormeon.com>
2019-03-29 14:04:43 +0000

    no message

--- prebid/golfwrx.js
+++ prebid/golfwrx.js
@@ -99,7 +99,8 @@ var timeoutMap = {
             	 // { bidder: 'appnexus',   params: { placementId: '13232392', video: { skippable: true, playback_method: ['auto_play_sound_off'] } } }, /* demo video placement, always returns a vid, only works client side, pretty sure having this on lifts Teads to bidding higher */
             	 // { bidder: 'districtm',  params: { placementId: 11937611 } }, /* 300x600 - 300x250 - 160x600 */ 
             	 { bidder: 'districtm',  params: { placementId: 11937611, video: { skippable: true, playback_method: ['auto_play_sound_off'] } } },
-            	 { bidder: 'districtmDMX',  params: { dmxid: 171413, memberid: 100041  } }, /* 300x600 - 300x250 - 160x600 */ 
+            	 // { bidder: 'districtmDMX',  params: { dmxid: 171413, memberid: 100041  } }, /* 300x600 - 300x250 - 160x600 */ 
+            	 { bidder: 'districtmDMX',  params: { dmxid: 345036, memberid: 100041  } }, /* outstream */ 
             	 { bidder: 'aol',        params: { placement: '4882886', network: '4436.1', server: 'adserver.adtech.de' } }, /* 300x250 */ 
             	 // { bidder: 'brealtime',  params: { placementId: '12002335' } },  /* 300x250 */   
             	 { bidder: 'conversant', params: { site_id: '118233' } },  /* 300x250 */ 
