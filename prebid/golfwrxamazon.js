9fa6882f785bd001c4cee680a4d22191c4c05408
david stanton <davidstanton@spormeon.com>
2019-04-23 14:47:13 +0100

    no message

--- prebid/golfwrxamazon.js
+++ prebid/golfwrxamazon.js
@@ -379,7 +379,7 @@ var timeoutMap = {
                       },
                   },
                 debug: true,
-                useBidCache: true,
+                useBidCache: USERBIDCACHE,
                 enableSendAllBids: false, // Default will be `true` as of 1.0
                 bidderSequence: 'random', // Default is random
                 	  publisherDomain: 'golfwrx.com',
