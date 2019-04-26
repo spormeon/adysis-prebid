bbe3e9587d2c57c3b4abc27b6223658c8f7dd128
david stanton <davidstanton@spormeon.com>
2019-04-26 17:21:12 +0100

    no message

--- prebid/golfwrxamazon.js
+++ prebid/golfwrxamazon.js
@@ -169,7 +169,7 @@ var timeoutMap = {
             	 },
             	bids: [
             	 { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
-            	 { bidder: 'appnexus',   params: { placementId: 11971351, allowSmallerSizes: true, usePaymentRule: true, video: {skippable: true, playback_method: ['auto_play_sound_off'], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playbackmethod: [2] } } }, /* one placementId for all sizes */
+            	 { bidder: 'appnexus',   params: { placementId: '11971351', allowSmallerSizes: true, usePaymentRule: true, video: {placementId: '11971351', skippable: true, playback_method: ['auto_play_sound_off'], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playbackmethod: [2] } } }, /* one placementId for all sizes */
             	 // { bidder: 'appnexus',   params: { placementId: '11971351', allowSmallerSizes: true, usePaymentRule: true, video: { skippable: true, playback_method: ['auto_play_sound_off'] } } }, /* one placementId for all sizes */
             	 // { bidder: 'appnexus',   params: { placementId: '13232392', video: { skippable: true, playback_method: ['auto_play_sound_off'] } } }, /* demo video placement, always returns a vid, only works client side, pretty sure having this on lifts Teads to bidding higher */
             	 // { bidder: 'districtm',  params: { placementId: 11937611, allowSmallerSizes: true } }, /* 300x600 - 300x250 - 160x600 */ 
