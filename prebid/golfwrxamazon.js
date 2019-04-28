6498b837a7dc0ba80bf954c92b8e4f77bd527b0c
david stanton <davidstanton@spormeon.com>
2019-04-29 00:21:45 +0100

    no message

--- prebid/golfwrxamazon.js
+++ prebid/golfwrxamazon.js
@@ -22,15 +22,15 @@ window.addEventListener("DOMContentLoaded",function(){
 
 //load up google gpt.js
 (function () {
-  var gads = document.createElement('script');
+  var gads = document.createElement("script");
   gads.async = true;
-  gads.type = 'text/javascript';
-  var useSSL = 'https:' == document.location.protocol;
-  gads.src = (useSSL ? 'https:' : 'http:') +
-          '//www.googletagservices.com/tag/js/gpt.js';
-  var node = document.getElementsByTagName('footerbid')[0];
+  gads.type = "text/javascript";
+  var useSSL = "https:" == document.location.protocol;
+  gads.src = (useSSL ? "https:" : "http:") +
+          "//www.googletagservices.com/tag/js/gpt.js";
+  var node = document.getElementsByTagName("footerbid")[0];
   node.parentNode.insertBefore(gads, node);
-})(); 
+})();
 
 //load up prebid.js,  I think we need to load this earlier
 (function() {
