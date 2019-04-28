3a72a8fe02182f36c06e954f4695f7f7dc4f9dfc
david stanton <davidstanton@spormeon.com>
2019-04-28 19:33:17 +0100

    no message

--- prebid/golfwrxamazon.js
+++ prebid/golfwrxamazon.js
@@ -172,7 +172,7 @@ var timeoutMap = {
             	 code: 'inreedvidSlot',
             	 mediaTypes: {
             		 banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
-            		 video:  { context: 'outstream', mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playerSize: [[550, 310]], minduration: 1,  maxduration: 30, protocols: [2,3], w:640, h:480, protocols: [2,3], playbackmethod: [2] } },  
+            		 video:  { context: 'outstream', rendererUrl: 'http://cdn.adnxs.com/renderer/video/ANOutstreamVideo.js', mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playerSize: [[550, 310]], minduration: 1,  maxduration: 30, protocols: [2,3], w:640, h:480, protocols: [2,3], playbackmethod: [2] } },  
             		 renderer: { url: 'http://cdn.adnxs.com/renderer/video/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCode, adResponse: bid.adResponse, }); 
             		 }   
             	 },
