9bc9a3cadc9eeb8b336238252ba1aea7e7830939
david stanton <davidstanton@spormeon.com>
2019-04-28 21:04:04 +0100

    no message

--- prebid/golfwrxamazon.js
+++ prebid/golfwrxamazon.js
@@ -386,7 +386,7 @@ var timeoutMap = {
                  s2sConfig: {
                      accountId: 'e31f627f-53a3-4288-9979-482d3c6ffc76',
                      enabled: true,
-                     bidders: ['sovrn','somoaudience'],
+                     bidders: ['sovrn','somoaudience','districtm'],
                      timeout: 1000,
                      adapter: 'prebidServer',
                      endpoint: 'https://prebid.adnxs.com/pbs/v1/openrtb2/auction',
