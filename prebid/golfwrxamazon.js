4a60bdacf735acfe3387f82e5b79e06d9f94a74a
david stanton <davidstanton@spormeon.com>
2019-04-28 16:49:52 +0100

    no message

--- prebid/golfwrxamazon.js
+++ prebid/golfwrxamazon.js
@@ -172,7 +172,7 @@ var timeoutMap = {
             	 code: 'inreedvidSlot',
             	 mediaTypes: {
             		 banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
-            		 video:  { context: 'outstream', mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playerSize: [550, 310], protocols: [2,3], playbackmethod: [2]  }, 
+            		 video:  { context: 'outstream', mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playerSize: [[550, 310]], minduration: 10,  maxduration: 30, protocols: [2,3], w:640, h:480, protocols: [2,3], playbackmethod: [2]  }, 
              	},
             	bids: [
             	 { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
@@ -211,7 +211,7 @@ var timeoutMap = {
             	 code: 'inreedvid1Slot',
             	 mediaTypes: {
             		 banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
-            		 video:  { context: 'outstream', mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playerSize: [550, 310], minduration: 10,  maxduration: 30, protocols: [2,3], w:640, h:480, playbackmethod: [2]  }, 
+            		 video:  { context: 'outstream', mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playerSize: [[550, 310]], minduration: 10,  maxduration: 30, protocols: [2,3], w:640, h:480, playbackmethod: [2]  }, 
             	},
             	bids: [
             	 { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
@@ -249,7 +249,7 @@ var timeoutMap = {
             	 code: 'inreedvid2Slot',
             	 mediaTypes: {
             	     banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
-            	     video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playbackmethod: [2] },
+            	     video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2] },
             	},
             	bids: [
             	 { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
@@ -287,7 +287,7 @@ var timeoutMap = {
             	 code: 'inreedvid3Slot',
             	 mediaTypes: {
             		 banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] }, 
-            		 video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playbackmethod: [2] },
+            		 video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2] },
             	},
             	bids: [
             	 { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
