9a6fa066247d5d90cc6063a0193ae3b9e7b7970d
david stanton <davidstanton@spormeon.com>
2019-04-29 00:03:54 +0100

    no message

--- prebid/golfwrxamazon.js
+++ prebid/golfwrxamazon.js
@@ -1,10 +1,8 @@
 // site config
 var site_config = {
-    refresh_rate: 40000,  //denoted in milliseonds 40secs=40000...
-    FAILSAFE_TIMEOUT: 5000,   //denoted in milliseonds 5secs=5000...
-    leaderboard_sizes_mobile: [[320, 50],[234,60]]
-  }; 
-
+refresh_rate: 40000,  //denoted in milliseonds 40secs=40000...
+FAILSAFE_TIMEOUT: 5000,   //denoted in milliseonds 5secs=5000...
+leaderboard_sizes_mobile: [[320, 50],[234,60]]};
 // site_config end
 
 // unruly player config //
@@ -12,12 +10,12 @@ window.top.unruly = window.top.unruly || {}
 window.top.unruly.native = window.top.unruly.native || {}
 window.top.unruly.native.onAdLoaded = () => {window.top.unruly.native.disclosureMessage = "";}
 
-//Slots to send to page, have to add 1 on to the child (number) because the div counts as 1 in the string 
+//Slots to send to page, have to add 1 on to the child (number)
 window.addEventListener("DOMContentLoaded",function(){
-    document.querySelector('#mvp-content-main p:nth-child(2)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvidSlot"></div></div><br>');
-    document.querySelector('#mvp-content-main p:nth-child(6)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
-    document.querySelector('#mvp-content-main p:nth-child(10)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
-    document.querySelector('#mvp-content-main p:nth-child(14)').insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid3Slot"></div></div><br>');
+    document.querySelector("#mvp-content-main p:nth-child(2)").insertAdjacentHTML("afterbegin","<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvidSlot"></div></div><br>");
+    document.querySelector("#mvp-content-main p:nth-child(6)").insertAdjacentHTML("afterbegin","<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid1Slot"></div></div><br>");
+    document.querySelector("#mvp-content-main p:nth-child(10)").insertAdjacentHTML("afterbegin","<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid2Slot"></div></div><br>");
+    document.querySelector("#mvp-content-main p:nth-child(14)").insertAdjacentHTML("afterbegin","<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid3Slot"></div></div><br>");
 });
 
 //load up google gpt.js
