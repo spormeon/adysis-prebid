eebe6c14b7a202991a3f142f6dafeb4f47a60d74
david stanton <davidstanton@spormeon.com>
2019-05-18 10:44:10 +0100

    no message

--- prebid/golfwrxamazon.js
+++ prebid/golfwrxamazon.js
@@ -1,3 +1,32 @@
+//  TIMEOUT MAP - 1000 def brings back bigger bids in Uk
+var timeoutMap = {
+0 : 1400,
+1 : 1400,
+2 : 1400,
+3 : 1400,
+4 : 1400,
+5 : 1400,
+6 : 1400,
+7 : 1400,
+8 : 1400,
+9 : 800,
+10 : 800,
+11 : 1400,
+12 : 1400,
+13 : 1400,
+14 : 1400,
+15 : 1000,
+16 : 1400,
+17 : 1400,
+18 : 1400,
+19 : 1400,
+20 : 1400,
+21 : 1400,
+22 : 1400,
+23 : 1400
+};
+var t = new Date().getUTCHours();
+PREBID_TIMEOUT = timeoutMap[t];
 // site config
 var site_config = {
     refresh_rate: PREBID_TIMEOUT*4.5,  //denoted in milliseonds 40secs=40000...
@@ -72,35 +101,7 @@ slots: [{
      });
  });
 // amazon bidder end
-//  1000 def brings back bigger bids in Uk
-var timeoutMap = {
-0 : 1400,
-1 : 1400,
-2 : 1400,
-3 : 1400,
-4 : 1400,
-5 : 1400,
-6 : 1400,
-7 : 1400,
-8 : 1400,
-9 : 800,
-10 : 800,
-11 : 1400,
-12 : 1400,
-13 : 1400,
-14 : 1400,
-15 : 1000,
-16 : 1400,
-17 : 1400,
-18 : 1400,
-19 : 1400,
-20 : 1400,
-21 : 1400,
-22 : 1400,
-23 : 1400
-};
-var t = new Date().getUTCHours();
-PREBID_TIMEOUT = timeoutMap[t];
+
 var usebidcacheMap = {
 0 : true,			   
 1 : false,
