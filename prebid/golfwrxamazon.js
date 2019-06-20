29832c5ca3808ad722d0099bb1b57d914c31cf4e
david stanton <davidstanton@spormeon.com>
2019-06-20 09:33:07 +0100

    no message

--- prebid/golfwrxamazon.js
+++ prebid/golfwrxamazon.js
@@ -166,9 +166,10 @@ console.log("user bid cache:", USERBIDCACHE );
     var adUnits = [
   //new ad unit block
      {
-      code: 
-    	  ['inreedvidSlot, inreedvid1Slot, inreedvid2Slot, inreedvid3Slot'],
-    	  
+      code: 'inreedvidSlot',
+      code: 'inreedvid1Slot',
+      code: 'inreedvid2Slot',
+      code: 'inreedvid3Slot',
       mediaTypes: {
        banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
        video:  { context: 'outstream', playerSize: [550, 310], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playbackmethod: [2] },
