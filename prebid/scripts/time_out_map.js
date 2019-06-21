c34259b85fef786b634650fc3f513215a2f76143
david stanton <davidstanton@spormeon.com>
2019-06-21 15:12:22 +0100

    no message

--- prebid/scripts/time_out_map.js
+++ prebid/scripts/time_out_map.js
@@ -28,3 +28,41 @@ var timeoutMap = {
 var t = new Date().getUTCHours();
 PREBID_TIMEOUT = timeoutMap[t];
 console.log("prebid timeout:", PREBID_TIMEOUT );
+
+//FLOOR_PRICE MAP - 1000 def brings back bigger bids in Uk
+var floorpriceMap = {
+0 : 0.20,
+1 : 0.21,
+2 : 0.22,
+3 : 0.23,
+4 : 0.24,
+5 : 0.25,
+6 : 0.16,
+7 : 0.17,
+8 : 0.18,
+9 : 0.19,
+10 : 0.20,
+11 : 0.31,
+12 : 0.32,
+13 : 0.33,
+14 : 0.34,
+15 : 0.35,
+16 : 0.36,
+17 : 0.37,
+18 : 0.38,
+19 : 0.39,
+20 : 0.40,
+21 : 0.41,
+22 : 0.42,
+23 : 0.43
+};
+var f = new Date().getUTCHours();
+FLOOR_PRICE = floorpriceMap[f];
+console.log("floor price:", FLOOR_PRICE );
+// site config
+var site_config = {
+    refresh_rate: PREBID_TIMEOUT*30,  //denoted in milliseonds 40secs=40000...
+    FAILSAFE_TIMEOUT: PREBID_TIMEOUT*1.5   //denoted in milliseonds 5secs=5000...
+    // floor_price: 1.00 //set a min floor price on bids to pressure higher bids
+  };
+// site_config end
