6c5a86472640b8f371d22c7b184be61fd75a84ff
david stanton <davidstanton@spormeon.com>
2019-06-21 12:47:46 +0100

    no message

--- prebid/scripts/main.js
+++ prebid/scripts/main.js
@@ -1,5 +1,7 @@
 //Load common code that includes config, then load the app logic for this page.
-
+requirejs(['./slots'], function (slots) {
+    requirejs(['timeout']);
+});
 
 //Slots to send to page, have to add 1 on to the child (number) because the div counts as 1 in the string 
 window.addEventListener("DOMContentLoaded",function(){
@@ -7,5 +9,5 @@ window.addEventListener("DOMContentLoaded",function(){
     document.querySelector("#mvp-content-main p:nth-child(6)").insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid1Slot"></div></div><br>');
     document.querySelector("#mvp-content-main p:nth-child(10)").insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid2Slot"></div></div><br>');
     document.querySelector("#mvp-content-main p:nth-child(14)").insertAdjacentHTML('afterbegin','<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid3Slot"></div></div><br>');
-    requirejs(['timeout']);
+
 });
