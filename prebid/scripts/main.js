5224260b0aeff099020a66b0e2ca4050df148352
david stanton <davidstanton@spormeon.com>
2019-06-24 17:53:08 +0100

    no message

--- prebid/scripts/main.js
+++ prebid/scripts/main.js
@@ -236,7 +236,4 @@ definitons: {
    });
 });
 
-assertive_custom_1 = USERBIDCACHE ? 'prebid_cache_enabled' : 'prebid_cache_disabled';
-assertive_custom_2 = FLOOR_PRICE;
-
 requirejs(['analytics_client']);
