f70d810f91581f2adb1f61783920d4355d8e89e0
david stanton <davidstanton@spormeon.com>
2019-06-24 21:35:33 +0100

    no message

--- prebid/scripts/main.js
+++ prebid/scripts/main.js
@@ -1,5 +1,32 @@
 requirejs(['prebid_gpt']);
 requirejs(['time_out_map']);
+//TIMEOUT MAP - 1000 def brings back bigger bids in Uk
+var timeoutMap = {
+0 : 1400,
+1 : 1400,
+2 : 1400,
+3 : 1400,
+4 : 1400,
+5 : 1400,
+6 : 1400,
+7 : 1400,
+8 : 1400,
+9 : 1200,
+10 : 1200,
+11 : 1400,
+12 : 1400,
+13 : 1400,
+14 : 1400,
+15 : 1000,
+16 : 1400,
+17 : 1400,
+18 : 1400,
+19 : 1400,
+20 : 1400,
+21 : 1400,
+22 : 1400,
+23 : 1400
+};
 var t = new Date().getUTCHours();
 PREBID_TIMEOUT = timeoutMap[t];
 console.log("prebid timeout:", PREBID_TIMEOUT );
@@ -47,35 +74,7 @@ window.top.unruly.native.onAdLoaded = () => {window.top.unruly.native.disclosure
 
 requirejs(['amazon_bidder']);
 
-var usebidcacheMap = {
-0 : true,			   
-1 : false,
-2 : true,
-3 : false,
-4 : true,
-5 : false,
-6 : true,
-7 : false,
-8 : true,
-9 : false,
-10 : true,
-11 : false,
-12 : true,
-13 : false,
-14 : true,
-15 : false,
-16 : true,
-17 : false,
-18 : true,
-19 : false,
-20 : true,
-21 : false,
-22 : true,
-23 : false
-};
-var u = new Date().getUTCHours();
-USERBIDCACHE = usebidcacheMap[u];
-console.log("user bid cache:", USERBIDCACHE );	
+requirejs(['user_bid_cache_map']);
 
 requirejs(['analytics_settings']);
 requirejs(['bidders']);   
