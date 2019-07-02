e93a55fb1f83317b79e4ef71e40e0969e06a1699
david stanton <davidstanton@spormeon.com>
2019-07-02 15:18:00 +0100

    no message

--- prebid/scripts/bidders.js
+++ prebid/scripts/bidders.js
@@ -3,7 +3,7 @@ var adUnits = [
      {
       code: 'inreedvidSlot',
       mediaTypes: {
-       banner: { sizes: [[550, 310],[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
+       banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
        video:  { context: 'outstream', playerSize: [550, 310], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playbackmethod: [2] },
       },
       bids: [
@@ -54,8 +54,8 @@ var adUnits = [
      {
      code: 'inreedvid1Slot',
      mediaTypes: {
-      banner: { sizes: [[550, 310],[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
-      video:  { context: 'outstream', mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playerSize: [[550, 310]], minduration: 10,  maxduration: 30, protocols: [2,3], w:640, h:480, playbackmethod: [2]  } 
+      banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
+      video:  { context: 'outstream', playerSize: [550, 310], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playbackmethod: [2] },
      },
       bids: [
       { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
@@ -103,7 +103,7 @@ var adUnits = [
       {
       code: 'inreedvid2Slot',
       mediaTypes: {
-       banner: { sizes: [[550, 310],[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
+       banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
        video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2] },
       },
        bids: [
@@ -152,7 +152,7 @@ var adUnits = [
        {
        code: 'inreedvid3Slot',
        mediaTypes: {
-        banner: { sizes: [[550, 310],[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] }, 
+        banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] }, 
         video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2] },
        },
         bids: [
