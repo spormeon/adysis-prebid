ea149969e331ff528533998b13bfedb8de186a66
david stanton <davidstanton@spormeon.com>
2019-07-12 09:02:44 +0100

    no message

--- prebid/golfwrxamazonA2.js
+++ prebid/golfwrxamazonA2.js
@@ -432,7 +432,7 @@ console.log("user bid cache:", USERBIDCACHE );
       s2sConfig: {
         accountId: 'e31f627f-53a3-4288-9979-482d3c6ffc76',
         enabled: true,
-        bidders: ['sovrn','somoaudience','districtm','rhythmone','pulsepoint','gamoshi'],
+        bidders: ['sovrn','somoaudience','districtm','rhythmone','pulsepoint','gamoshi','rubicon','openx','sharethrough','unruly','33across','ix'],
         timeout: PREBID_TIMEOUT/2,
         adapter: 'prebidServer',
         endpoint: 'https://prebid.adnxs.com/pbs/v1/openrtb2/auction',
