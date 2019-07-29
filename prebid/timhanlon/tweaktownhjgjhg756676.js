ee8c07a3dc24fbe83281b0147e0249a2a69016da
david stanton <davidstanton@spormeon.com>
2019-07-29 21:14:10 +0100

    no message

--- prebid/timhanlon/tweaktownhjgjhg756676.js
+++ prebid/timhanlon/tweaktownhjgjhg756676.js
@@ -441,7 +441,7 @@ console.log("user bid cache:", USERBIDCACHE );
       s2sConfig: {
         accountId: 'e31f627f-53a3-4288-9979-482d3c6ffc76',
         enabled: true,
-        bidders: ['sovrn','somoaudience','rhythmone','pulsepoint','gamoshi','rubicon','ix','openx','unruly'],
+        bidders: ['sovrn','somoaudience','rhythmone','pulsepoint','gamoshi','rubicon','33across','ix','openx','unruly'],
         timeout: PREBID_TIMEOUT/2,
         adapter: 'prebidServer',
         endpoint: 'https://prebid.adnxs.com/pbs/v1/openrtb2/auction',
