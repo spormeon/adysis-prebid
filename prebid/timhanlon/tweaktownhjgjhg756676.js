1538f27c6017fff1c684aee643b75a8be5999d73
david stanton <davidstanton@spormeon.com>
2019-07-30 11:11:38 +0100

    no message

--- prebid/timhanlon/tweaktownhjgjhg756676.js
+++ prebid/timhanlon/tweaktownhjgjhg756676.js
@@ -178,7 +178,7 @@ console.log("user bid cache:", USERBIDCACHE );
       code: 'inreedvid4Slot',
       mediaTypes: {
        banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50],[1,1]] },
-       video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playbackmethod: [2] }
+       video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', "video/webm"], playbackmethod: [2], minduration: 10,  maxduration: 30, protocols: [2,3] }
       },
       bids: [
           { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
