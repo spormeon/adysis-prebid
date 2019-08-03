7b39ce60e3320db09cd303a4341f5baed5abc86a
david stanton <davidstanton@spormeon.com>
2019-08-03 11:25:36 +0100

    no message

--- prebid/tweaktownhjgjhg756676native.js
+++ prebid/tweaktownhjgjhg756676native.js
@@ -178,7 +178,7 @@ console.log("user bid cache:", USERBIDCACHE );
       code: 'inreedvid4Slot',
       mediaTypes: {
     	  native: { image: { required: true, sendId: true, aspect_ratios: [{ min_width: 300, ratio_width: 3, ratio_height: 2, }] }, title: { required: true, len: 55 }, sponsoredBy: { required: true }, clickUrl: { required: true, sendId: true } }
-      }
+      },
       bids: [
           { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
           { bidder: 'appnexus',   params: { placementId: '11962910', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_on'] } } }, /* one placementId for all sizes */
@@ -232,7 +232,7 @@ console.log("user bid cache:", USERBIDCACHE );
      code: 'inreedvid5Slot',
      mediaTypes: {   
    	  native: { image: { required: true, sendId: true, aspect_ratios: [{ min_width: 300, ratio_width: 3, ratio_height: 2, }] }, title: { required: true, len: 55 }, sponsoredBy: { required: true }, clickUrl: { required: true, sendId: true } }
-     }
+     },
      bids: [
          { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
          { bidder: 'appnexus',   params: { placementId: '11962910', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_on'] } } }, /* one placementId for all sizes */
