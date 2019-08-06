09a6be513f287f7f58db3b18b868ae760825e8ec
david stanton <davidstanton@spormeon.com>
2019-08-06 11:46:46 +0100

    no message

--- prebid/tweaktownhjgjhg756676native.js
+++ prebid/tweaktownhjgjhg756676native.js
@@ -178,10 +178,18 @@ console.log("user bid cache:", USERBIDCACHE );
       code: 'inreedvid4Slot',
       mediaTypes: {
           native: { image: { sizes: [300, 100], sendId: true }, title: { len: 50 }, sponsoredBy: { required: true }, clickUrl: { required: true, sendId: true } },
-          banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50]] },
           video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2], minduration: 0,  maxduration: 45, protocols: [1,2,3,4,5,6,7,8,9,10], linearity: 1, api: [1,2,3,4,5,6] }
          },
-         
+         renderer: {
+             url: 'http://cdn.adnxs.com/renderer/video/ANOutstreamVideo.js',
+             render: function (bid) {
+                 ANOutstreamVideo.renderAd({
+                     targetId: bid.adUnitCode,
+                     adResponse: bid.adResponse,
+                 });
+             }
+         },
+         renderer: { options: { adText: 'Advertisement' } },
       bids: [
           { bidder: 'teads',      params: { placementId: '75853', pageId: '87372' } },
           { bidder: 'appnexus',   params: { placementId: '11962910', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_on'] } } }, /* one placementId for all sizes */
