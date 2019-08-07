097dca256da3d85fe72dc2995087f4382f404cef
david stanton <davidstanton@spormeon.com>
2019-08-07 10:09:14 +0100

    no message

--- prebid/tweaktownhjgjhg756676native.js
+++ prebid/tweaktownhjgjhg756676native.js
@@ -177,7 +177,8 @@ console.log("user bid cache:", USERBIDCACHE );
      {
       code: 'inreedvid4Slot',
       mediaTypes: {
-          native: { image: { sizes: [300, 100], sendId: true }, title: { len: 50 }, sponsoredBy: { required: true }, clickUrl: { required: true, sendId: true } },
+    	  banner: { sizes: [[300, 250],[728, 90],[250, 250],[468, 60],[320, 50],[1,1]] },
+    	  native: { image: { sizes: [300, 100], sendId: true }, title: { len: 50 }, sponsoredBy: { required: true }, clickUrl: { required: true, sendId: true } },
           video:  { context: 'outstream', playerSize: [[550, 310]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2], minduration: 0,  maxduration: 45, protocols: [1,2,3,4,5,6,7,8,9,10], linearity: 1, api: [1,2,3,4,5,6] }
          },
          renderer: {
