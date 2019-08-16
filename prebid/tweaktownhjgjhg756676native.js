45caa9a3455485491461c025e5bf46a3b6f07255
david stanton <davidstanton@spormeon.com>
2019-08-16 18:22:44 +0100

    no message

--- prebid/tweaktownhjgjhg756676native.js
+++ prebid/tweaktownhjgjhg756676native.js
@@ -755,7 +755,7 @@ adysis: { bidCpmAdjustment : function(bidCpm){ return "+c.cpm+" * 2;} },
        pubcid: {expInterval: 525600},
        currency: { 'adServerCurrency': "GBP", 'granularityMultiplier': 1, 'conversionRateFile': 'https://cdn.jsdelivr.net/gh/prebid/currency-file@1/latest.json', },
        sizeConfig: [
-         { mediaQuery: '(min-width: 769px)', 'sizesSupported': [[580, 400],[550, 310], [728, 90], [468, 60], [336, 280], [320, 100], [320, 50], [300, 250], [250, 250], [1, 1], [0, 0]], 'labels': ['desktop'] },
+         { mediaQuery: '(min-width: 769px)', 'sizesSupported': [[580, 400],[550, 310], [728, 90], [468, 60], [336, 280], [320, 100], [320, 50], [300, 250], [250, 250], [1, 1]], 'labels': ['desktop'] },
          { mediaQuery: '(min-width: 500px) and (max-width: 768px)', sizesSupported: [[550, 310], [580, 400], [728, 90],  [468, 60], [320, 50], [320, 100], [336, 280], [300, 250], [250, 250], [1, 1]], labels: ['tablet'] },
          { mediaQuery: '(min-width: 1px) and (max-width: 499px)', sizesSupported: [[550, 310], [300, 250], [250, 250], [320, 50], [320, 100], [336, 280], [1, 1]], labels: ['phone'] }
        ]
@@ -854,7 +854,7 @@ definitons: {
     },
     sizeMappings: {
      mappinginreedvidslot: [
-     [[1024, 768],[[1,1],[550,310],[728,90],[300,250],[250,250],[468,60],[320,50],[336,280],[580,400],[320,100],[0,0]]],
+     [[1024, 768],[[1,1],[550,310],[728,90],[300,250],[250,250],[468,60],[320,50],[336,280],[580,400],[320,100]]],
      [[768, 500],[[728,90],[300,250],[250,250],[468,60],[320,50],[1,1],[550,310],[336,280],[580,400],[320,100]]],
      [[1, 1],[[300,250],[250,250],[320,50],[320, 100],[336, 280],[468, 90],[1,1]]],
     ]
