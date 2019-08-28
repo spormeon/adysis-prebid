cd50bc0693894403390abeba2f16c51ccabfe8f7
david stanton <davidstanton@spormeon.com>
2019-08-28 12:35:11 +0100

    no message

--- prebid/preconnect.js
+++ prebid/preconnect.js
@@ -5,5 +5,5 @@ pclink1.rel = "preconnect";
 pclink1.href = "https://www.googletagservices.com/";
 pclink1.crossorigin;
 var nodepclink1 = document.getElementsByTagName("footerbid1")[0];
-nodepclink1.insertBefore(pclink1, nodepclink1);
+nodepclink1.insertBefore(pclink1, nodepclink1.firstChild);
 })();
