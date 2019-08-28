5d55e9d9d6b8db02401d6d10d9d478f1f3a7ee11
david stanton <davidstanton@spormeon.com>
2019-08-28 12:31:52 +0100

    no message

--- prebid/preconnect.js
+++ prebid/preconnect.js
@@ -3,7 +3,7 @@
 var pclink1 = document.createElement("link");
 pclink1.rel = "preconnect";
 pclink1.href = "https://www.googletagservices.com/";
-pclink1.crossorigin = crossorigin;
+pclink1.crossorigin;
 var nodepclink1 = document.getElementsByTagName("footerbid1")[0];
 nodepclink1.insertBefore(pclink1, nodepclink1);
 })();
