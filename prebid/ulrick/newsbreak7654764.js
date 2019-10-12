03f1bb9ad36a2f3d40d4c78aa8b0feddee8c41ca
david stanton <davidstanton@spormeon.com>
2019-10-12 11:44:34 +0100

    no message

--- prebid/ulrick/newsbreak7654764.js
+++ prebid/ulrick/newsbreak7654764.js
@@ -1453,6 +1453,7 @@ mappingleaderslot: [
 });
 assertive_custom_1 = USERBIDCACHE ? 'prebid_cache_enabled' : 'prebid_cache_disabled';
 assertive_custom_2 = FLOOR_PRICE;
+assertive_custom_3 = site_config.refresh_rate;
 
 /*  ####################################
  *  #    ASSERTIVE ANALYTICS CLIENT    #
