bc7d805070fec386085dd03d26ae95ed7150e96a
david stanton <davidstanton@spormeon.com>
2019-10-17 10:21:39 +0100

    no message

--- prebid/ulrick/newsbreak7654764B.js
+++ prebid/ulrick/newsbreak7654764B.js
@@ -792,6 +792,9 @@ googletag.cmd.push(function () {
 // the order below determines the order on the page //
     })(window.googletag, window.adyjs, {
 definitons: {
+inreedvid11Slot: { adUnitPath: "/1001824/adp100001/adp100001H", size: "mappinginreedvidslot", sizeMapping: "mappingstickyslot", timeout: site_config.refresh_rate, },
+inreedvid12Slot: { adUnitPath: "/1001824/adp100001/adp100001I", size: "mappinginreedvidslot", sizeMapping: "mappingstickyslot", timeout: site_config.refresh_rate, },
+inreedvid13Slot: { adUnitPath: "/1001824/adp100001/adp100001J", size: "mappinginreedvidslot", sizeMapping: "mappingfooterslot", timeout: site_config.refresh_rate, },
 inreedvid8Slot: { adUnitPath: "/1001824/adp100001/adp100001E", size: "mappingleaderslot", sizeMapping: "mappingleaderslot", timeout: site_config.refresh_rate, },
 inreedvid4Slot: { adUnitPath: "/1001824/adp100001/adp100001A", size: "mappingleaderslot", sizeMapping: "mappingleaderslot", timeout: site_config.refresh_rate, },
 inreedvid5Slot: { adUnitPath: "/1001824/adp100001/adp100001B", size: "mappingmenu1slot", sizeMapping: "mappingmenu1slot", timeout: site_config.refresh_rate, },
@@ -799,9 +802,6 @@ inreedvid9Slot: { adUnitPath: "/1001824/adp100001/adp100001F", size: "mappingmen
 inreedvid6Slot: { adUnitPath: "/1001824/adp100001/adp100001C", size: "mappinginreedvidslot", sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
 inreedvid10Slot: { adUnitPath: "/1001824/adp100001/adp100001G", size: "mappinginreedvidslot", sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
 inreedvid7Slot: { adUnitPath: "/1001824/adp100001/adp100001D", size: "mappinginreedvidslot", sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
-inreedvid11Slot: { adUnitPath: "/1001824/adp100001/adp100001H", size: "mappinginreedvidslot", sizeMapping: "mappingstickyslot", timeout: site_config.refresh_rate, },
-inreedvid12Slot: { adUnitPath: "/1001824/adp100001/adp100001I", size: "mappinginreedvidslot", sizeMapping: "mappingstickyslot", timeout: site_config.refresh_rate, },
-inreedvid13Slot: { adUnitPath: "/1001824/adp100001/adp100001J", size: "mappinginreedvidslot", sizeMapping: "mappingfooterslot", timeout: site_config.refresh_rate, },
 },
 sizeMappings: {
 mappinginreedvidslot: [
