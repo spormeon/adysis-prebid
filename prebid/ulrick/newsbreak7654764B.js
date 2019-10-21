881f480856480c8a4db050e11ab53f6d8de276d6
david stanton <davidstanton@spormeon.com>
2019-10-21 11:32:44 +0100

    no message

--- prebid/ulrick/newsbreak7654764B.js
+++ prebid/ulrick/newsbreak7654764B.js
@@ -276,7 +276,7 @@ console.log("user bid cache:", USERBIDCACHE );
     // { bidder: 'ucfunnel', params: { adid: 'ad-E2BBB7E7B69BD226F93D69A83686264' } }
     { bidder: 'beachfront', params: { video: { bidfloor: 0.01, appId: '83d77824-262e-4d13-ae0e-56f8f54bf934', mimes: [ 'video/mp4', 'application/javascript' ] }, banner: { bidfloor: 0.01, appId: '46f09c62-f3f1-4ead-f957-f91964be6f02' } } },
     // { bidder: 'beachfront', params: { bidfloor: 0.01, appId: '11bc5dd5-7421-4dd8-c926-40fa653bec76' } }, /* video for s2s bidding */
-    { bidder: 'beachfront', params: { bidfloor: 0.01, appId: '3b16770b-17af-4d22-daff-9606bdf2c9c3' } }, /* banners for s2s bidding */
+    // { bidder: 'beachfront', params: { bidfloor: 0.01, appId: '3b16770b-17af-4d22-daff-9606bdf2c9c3' } }, /* banners for s2s bidding */
     //{ bidder: 'cedato', params: { player_id: '1895193152' , bidfloor: 0.01 } },
     { bidder: 'adysis', params: { placementId: '11962910', allowSmallerSizes: true } }, /* one placementId for all sizes */
     //{ bidder: 'smartyads', params: { placementId: '6368' } }, /* 300x250 */
