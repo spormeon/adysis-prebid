84a8c437e9f163f6863eb85faa9d8bdb1c171a33
david stanton <davidstanton@spormeon.com>
2019-10-22 20:50:46 +0100

    no message

--- prebid/ulrick/newsbreak7654764B.js
+++ prebid/ulrick/newsbreak7654764B.js
@@ -243,7 +243,7 @@ console.log("user bid cache:", USERBIDCACHE );
     //{ bidder: "ix", params: { siteId: "261017", size: [300, 250], video: { skippable: true, mimes: [ 'video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm' ], minduration: 0, maxduration: 45, protocols: [1,2,3,4,5,6,7,8,9,10] } } }, /* id 261017 golfwrx id */
     { bidder: 'emx_digital', params: { tagid: '77460', video: { skippable: true, playback_method: ['auto_play_sound_off'] } } }, /* outstream */
     { bidder: 'brealtime', params: { placementId: '16714331', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_off'] } } },  /* video placement */
-    { bidder: 'smartrtb', params: { zoneId: "I0Mnk7dqNXKrCdjpgzp5", forceBid: true } }
+    { bidder: 'smartrtb', params: { zoneId: "I0Mnk7dqNXKrCdjpgzp5" } }
     ] //./bids
     };
     // BANNER BIDDER LIST
