2ea644438e53dc809547adae3f760874abaf2ce4
david stanton <davidstanton@spormeon.com>
2019-10-29 15:51:51 +0000

    no message

--- prebid/requirejs/lib/startbids.js
+++ prebid/requirejs/lib/startbids.js
@@ -3,9 +3,3 @@ var googletag = googletag || {};
      googletag.cmd.push(function() {
      googletag.pubads().disableInitialLoad();
     });
-    
-     var adyjs = adyjs || {};
-     adyjs.que = adyjs.que || [];
-     adyjs.que.push(function() {
-     adyjs.addAdUnits(adUnits);
-     //adyjs.addAdUnits(videoAdUnit); // add your ad units to the bid request
