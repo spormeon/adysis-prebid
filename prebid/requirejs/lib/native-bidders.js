c5723e170f4b89e8af4fb7dfb4b6a6db1431c151
david stanton <davidstanton@spormeon.com>
2019-10-29 15:17:42 +0000

    no message

---
+++ prebid/requirejs/lib/native-bidders.js
@@ -0,0 +1,15 @@
+//  NATIVE BIDDER LIST
+    var native_bidders = {
+    bids: [	
+    { bidder: 'appnexus', params: { placementId: '11962910', allowSmallerSizes: true } }, /* one placementId for all sizes */
+    { bidder: 'districtm', params: { placementId: 11937611, allowSmallerSizes: true } },
+    { bidder: 'pulsepoint', params: { cf: '300X250', cp: '561446', ct: '602639' } }, /* 300x250 */
+    { bidder: 'rhythmone', params: { placementId: '76184', zone: '1r', path: 'mvo' } }, /* one placementId for all sizes */
+    { bidder: 'adysis', params: { placementId: '11962910', allowSmallerSizes: true } }, /* one placementId for all sizes */
+    //{ bidder: 'smartyads', params: { placementId: '12335' } }, /* native */
+    { bidder: 'oftmedia', params: { placementId: '16137883', allowSmallerSizes: true } },
+    { bidder: 'openx', params: { unit: '539181725', delDomain: 'freestar-d.openx.net', openrtb: { imp: [{ video: { mimes: ['video/x-flv, video/mp4, application/x-shockwave-flash, application/javascript, video/webm'] } }] } } },
+    //{ bidder: 'colossusssp', params: { placementId: '35324' } } /* native */
+    { bidder: 'brealtime', params: { placementId: '16717366', allowSmallerSizes: true } } /* one placementId for all sizes */
+    ] //./bids
+    };
