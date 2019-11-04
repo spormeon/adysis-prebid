a1b2b0b4cb173ed788fd15e58041a238cdfa38ec
david stanton <davidstanton@spormeon.com>
2019-11-04 18:08:39 +0000

    assertive Hmac and UserID, embed test

--- prebid/assertembed.js
+++ prebid/assertembed.js
@@ -1,17 +1,17 @@
 let options = {
-        entities: ['k4nkHC4MwMa3NqC7o'],
+        entities: ['FHkAkgiDWrXm4dZFw'],
         timezone: 'UTC'
     };
     let auth = {
-        userId: '6SQYokfDH3X8g5pFc',
-        entities: ['k4nkHC4MwMa3NqC7o'],
+        userId: 'nKpSYRQrPPvFne4Mt',
+        entities: ['FHkAkgiDWrXm4dZFw'],
         validUntil: validUntil ? validUntil : (+ new Date() + (1000 * 60 * 60 * 24 * 7))
     };
 
     options = JSON.stringify(options);
     auth = JSON.stringify(auth);
 
-    const hmac = crypto.createHmac('sha256', 'mySuperAwesomeHMACSecret').update(auth).digest('hex');
+    const hmac = crypto.createHmac('sha256', 'SlrIDPOBy5OtkFl8QSp0daks7DmNitwg7VesB8Sj4Th').update(auth).digest('hex');
     const params = 'options=' + encodeURIComponent(options) + '&auth=' + encodeURIComponent(auth) + '&hmac=' + hmac;
 
     const url = 'https://suite.assertiveyield.com/' + 'embed/view/ad-units?' + params;
