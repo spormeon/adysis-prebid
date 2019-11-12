d1f978416efd7206e68db3a0e3791ed4daaad8d4
david stanton <davidstanton@spormeon.com>
2019-11-12 12:43:08 +0000

    no message

--- prebid/requirejs/app/main.js
+++ prebid/requirejs/app/main.js
@@ -7,10 +7,11 @@ require(["unrulyconfig"], function (unrulyconfig) {
 // require(["amazonbidder"], function (amazonbidder) {
 require(["bidcachemap"], function (bidcachemap) {
 require(["assertiveconfig"], function (assertiveconfig) {
+	require(["adunitsconfig"], function (adunitsconfig) {
 require(["nativebidders"], function (nativebidders) {
 require(["outstreambidders"], function (outstreambidders) {
 require(["bannerbidders"], function (bannerbidders) {
-require(["adunitsconfig"], function (adunitsconfig) {
+
 require(["custombuckets"], function (custombuckets) {
 require(["prebidconfig"], function (prebidconfig) {
 require(["assertivecustom"], function (assertivecustom) {
