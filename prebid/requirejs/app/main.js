2a9a53899877490aa7491e251f437ef44e086f1d
david stanton <davidstanton@spormeon.com>
2019-11-12 11:15:48 +0000

    no message

--- prebid/requirejs/app/main.js
+++ prebid/requirejs/app/main.js
@@ -1,2 +1,39 @@
-require(["prebid", "gpt", "preconnect", "bidfilter", "timeoutmap", "floorpricemap", "siteconfig", "unrulyconfig", "amazonbidder", "bidcachemap", "assertiveconfig", "nativebidders", "outstreambidders", "bannerbidders", "adunitsconfig", "custombuckets", "prebidconfig", "assertivecustom", "assertive"], function (prebid, gpt, preconnect, bidfilter, timeoutmap, floorpricemap, siteconfig, unrulyconfig, amazonbidder, bidcachemap, assertiveconfig, nativebidders, outstreambidders, bannerbidders, adunitsconfig, custombuckets, prebidconfig, assertivecustom, assertive) {
+require(["prebid"], function (prebid) {
+require(["gpt"], function (gpt) {
+require(["preconnect"], function (preconnect) {
+require(["bidfilter"], function (bidfilter) {
+require(["timeoutmap"], function (timeoutmap) {
+require(["floorpricemap"], function (floorpricemap) {
+require(["siteconfig"], function (siteconfig) {
+require(["unrulyconfig"], function (unrulyconfig) {
+require(["amazonbidder"], function (amazonbidder) {
+require(["bidcachemap"], function (bidcachemap) {
+require(["assertiveconfig"], function (assertiveconfig) {
+require(["nativebidders"], function (nativebidders) {
+require(["outstreambidders"], function (outstreambidders) {
+require(["bannerbidders"], function (bannerbidders) {
+require(["adunitsconfig"], function (adunitsconfig) {
+require(["custombuckets"], function (custombuckets) {
+require(["prebidconfig"], function (prebidconfig) {
+require(["assertivecustom"], function (assertivecustom) {
+require(["assertive"], function (assertive) {
+console.log('all deps loaded in order');
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
 });
