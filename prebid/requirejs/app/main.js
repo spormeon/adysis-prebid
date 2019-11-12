838c236895b80dadfb486246ce9747053006e37e
david stanton <davidstanton@spormeon.com>
2019-11-12 12:50:33 +0000

    no message

--- prebid/requirejs/app/main.js
+++ prebid/requirejs/app/main.js
@@ -1,21 +1,22 @@
 // require(["slotsonpage"], function (slotsonpage) {
-require(["gpt"], function (gpt) {
+
 require(["timeoutmap"], function (timeoutmap) {
 require(["floorpricemap"], function (floorpricemap) {
 require(["siteconfig"], function (siteconfig) {
-require(["unrulyconfig"], function (unrulyconfig) {
+//require(["unrulyconfig"], function (unrulyconfig) {
 // require(["amazonbidder"], function (amazonbidder) {
 require(["bidcachemap"], function (bidcachemap) {
 require(["assertiveconfig"], function (assertiveconfig) {
-	require(["adunitsconfig"], function (adunitsconfig) {
+	
 require(["nativebidders"], function (nativebidders) {
 require(["outstreambidders"], function (outstreambidders) {
 require(["bannerbidders"], function (bannerbidders) {
-
+	require(["adunitsconfig"], function (adunitsconfig) {
 require(["custombuckets"], function (custombuckets) {
 require(["prebidconfig"], function (prebidconfig) {
 require(["assertivecustom"], function (assertivecustom) {
 require(["prebid"], function (prebid) {
+	require(["gpt"], function (gpt) {
 require(["assertive"], function (assertive) {
 require(["bidfilter"], function (bidfilter) {
 require(["preconnect"], function (preconnect) {
@@ -37,6 +38,6 @@ console.log('all deps loaded in order');
 });
 });
 });
-});
+//});
 // });
 // });
