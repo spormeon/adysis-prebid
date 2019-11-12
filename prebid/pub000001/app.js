83ea9d18fcb37a5e9392647de124f18e0ad760f0
david stanton <davidstanton@spormeon.com>
2019-11-12 15:24:09 +0000

    no message

--- prebid/pub000001/app.js
+++ prebid/pub000001/app.js
@@ -1,5 +1,4 @@
 // For any third party dependencies, like jQuery, place them in the lib folder.
-
 // Configure loading modules from the lib directory,
 // except for 'app' ones, which are in a sibling
 // directory.
@@ -9,7 +8,6 @@ requirejs.config({
         app: '../app'
     }
 });
-
 // Start loading the main app file. Put all of
 // your application logic in there.
 requirejs(['app/main']);
