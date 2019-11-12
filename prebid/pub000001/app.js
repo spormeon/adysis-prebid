db41026aeeb027bf009d0a14ac0319ec7c66f060
david stanton <davidstanton@spormeon.com>
2019-11-12 15:42:28 +0000

    no message

--- prebid/pub000001/app.js
+++ prebid/pub000001/app.js
@@ -1,14 +1,9 @@
-// For any third party dependencies, like jQuery, place them in the lib folder.
-// Configure loading modules from the lib directory,
-// except for 'app' ones, which are in a sibling
-// directory.
 requirejs.config({
-    baseUrl: 'pub000001/lib',
-    paths: {
-        app: '../app'
-    }
+baseUrl: 'pub000001/lib',
+paths: {
+app: '../app'
+}
 });
-// Start loading the main app file. Put all of
 // your application logic in there.
 //require(["app/slotsonpage"], function (slotsonpage) {
 require(["app/timeoutmap"], function (timeoutmap) {
@@ -17,7 +12,7 @@ require(["app/siteconfig"], function (siteconfig) {
 // require(["app/unrulyconfig"], function (unrulyconfig) {
 // require(["app/amazonbidder"], function (amazonbidder) {
 require(["app/bidcachemap"], function (bidcachemap) {
-require(["app/assertiveconfig"], function (assertiveconfig) {	
+require(["app/assertiveconfig"], function (assertiveconfig) {
 require(["app/nativebidders"], function (nativebidders) {
 require(["app/outstreambidders"], function (outstreambidders) {
 require(["app/bannerbidders"], function (bannerbidders) {
@@ -26,9 +21,9 @@ require(["app/custombuckets"], function (custombuckets) {
 require(["app/prebidconfig"], function (prebidconfig) {
 require(["app/assertivecustom"], function (assertivecustom) {
 require(["prebid"], function (prebid) {
-require(["gpt"], function (gpt) {
 require(["assertive"], function (assertive) {
 require(["bidfilter"], function (bidfilter) {
+require(["gpt"], function (gpt) {
 // require(["preconnect"], function (preconnect) {
 console.log('all deps loaded in order');
 });
