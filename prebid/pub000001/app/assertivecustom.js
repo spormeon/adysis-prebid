809f9fa7a34d055b94253109f5b3ffc927c94ead
david stanton <davidstanton@spormeon.com>
2019-11-16 10:30:35 +0000

    no message

--- prebid/pub000001/app/assertivecustom.js
+++ prebid/pub000001/app/assertivecustom.js
@@ -1,4 +1,4 @@
 assertive_custom_1 = USERBIDCACHE ? 'prebid_cache_enabled' : 'prebid_cache_disabled';
 assertive_custom_2 = FLOOR_PRICE;
 assertive_custom_3 = site_config.refresh_rate;
-// assertive_custom_4 = function (bidResponse) { return bidResponse.source; };
+assertive_custom_4 = { val: function (bidResponse) { return bidResponse.source; } };
--- prebid/pub000001/app/prebidconfig.js
+++ prebid/pub000001/app/prebidconfig.js
@@ -70,8 +70,9 @@ rubicon:   { bidCpmAdjustment : function(bidCpm){ if(bidCpm < FLOOR_PRICE){ retu
 emx_digital:   { bidCpmAdjustment : function(bidCpm){ if(bidCpm < FLOOR_PRICE){ return 0;}return bidCpm * 1.00; } },
 //decenterads:   { bidCpmAdjustment : function(bidCpm){ if(bidCpm < FLOOR_PRICE){ return 0;}return bidCpm * 1.00; } },
 smartrtb:   { bidCpmAdjustment : function(bidCpm){ if(bidCpm < FLOOR_PRICE){ return 0;}return bidCpm * 1.00; } },
-adysis: { bidCpmAdjustment : function(bidCpm){ return "+c.cpm+" * 2;} },
-adysis: { bidCpmAdjustment : function(bidCpm){ return 1.39;} }
+//adysis: { bidCpmAdjustment : function(bidCpm){ return "+c.cpm+" * 2;} },
+adysis: { bidCpmAdjustment : function(bidCpm){ return 0.68;} }
+//adysis: { bidCpmAdjustment : function(bidCpm){ if(bidCpm < 300 ){ return 0.68; } },
 };
     adyjs.setConfig({
     	rubicon: {singleRequest: true},
