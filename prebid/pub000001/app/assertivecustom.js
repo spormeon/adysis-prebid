5f01abd0bb6b9bde60c3bca34e642c3825ef38a4
david stanton <davidstanton@spormeon.com>
2019-11-16 10:44:16 +0000

    no message

--- prebid/pub000001/app/assertivecustom.js
+++ prebid/pub000001/app/assertivecustom.js
@@ -2,4 +2,8 @@ assertive_custom_1 = USERBIDCACHE ? 'prebid_cache_enabled' : 'prebid_cache_disab
 assertive_custom_2 = FLOOR_PRICE;
 assertive_custom_3 = site_config.refresh_rate;
 assertive_custom_4 = function (bidResponse) { return bidResponse.source; };
-console.log("Bid Source", bidResponse.source);
+//console.log("Bid Source", bidResponse.source);
+
+var source = function (bidResponse) { return bidResponse.source; };
+PREBID_SOURCE = source;
+console.log("prebid source:", PREBID_SOURCE );
