05efc2a88b0b68644657d3fac34c80f82aa733d8
david stanton <davidstanton@spormeon.com>
2019-11-21 15:10:13 +0000

    no message

---
+++ prebid/pub000002/app.js
@@ -0,0 +1,47 @@
+requirejs.config({
+baseUrl: 'pub000001/lib',
+paths: { app: '../app' }
+});
+// your application logic in there.
+//require(["app/slotsonpage"], function (slotsonpage) {
+require(["prebid2.40.0adyjs"], function (prebid) {
+require(["gpt"], function (gpt) {
+require(["bidfilter"], function (bidfilter) {
+require(["app/timeoutmap"], function (timeoutmap) {
+require(["app/floorpricemap"], function (floorpricemap) {
+require(["app/siteconfig"], function (siteconfig) {
+// require(["app/unrulyconfig"], function (unrulyconfig) {
+// require(["app/amazonbidder"], function (amazonbidder) {
+require(["app/bidcachemap"], function (bidcachemap) {
+require(["app/assertiveconfig"], function (assertiveconfig) {
+require(["app/nativebidders"], function (nativebidders) {
+require(["app/outstreambidders"], function (outstreambidders) {
+require(["app/bannerbidders"], function (bannerbidders) {
+require(["app/adunitsconfig"], function (adunitsconfig) {
+require(["app/custombuckets"], function (custombuckets) {
+require(["app/prebidconfig"], function (prebidconfig) {
+require(["app/assertivecustom"], function (assertivecustom) {
+require(["assertive"], function (assertive) {
+require(["preconnect"], function (preconnect) {	
+//require(["app/addata"], function (preconnect) {	
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+//});
+// });
+// });
+// });
---
+++ prebid/pub000002/app/main.js
@@ -0,0 +1,41 @@
+// require(["app/slotsonpage"], function (slotsonpage) {
+require(["app/timeoutmap"], function (timeoutmap) {
+require(["app/floorpricemap"], function (floorpricemap) {
+require(["app/siteconfig"], function (siteconfig) {
+// require(["app/unrulyconfig"], function (unrulyconfig) {
+// require(["app/amazonbidder"], function (amazonbidder) {
+require(["app/bidcachemap"], function (bidcachemap) {
+require(["app/assertiveconfig"], function (assertiveconfig) {	
+require(["app/nativebidders"], function (nativebidders) {
+require(["app/outstreambidders"], function (outstreambidders) {
+require(["app/bannerbidders"], function (bannerbidders) {
+require(["app/adunitsconfig"], function (adunitsconfig) {
+require(["app/custombuckets"], function (custombuckets) {
+require(["app/prebidconfig"], function (prebidconfig) {
+require(["app/assertivecustom"], function (assertivecustom) {
+require(["prebid"], function (prebid) {
+require(["gpt"], function (gpt) {
+require(["assertive"], function (assertive) {
+require(["bidfilter"], function (bidfilter) {
+// require(["preconnect"], function (preconnect) {
+console.log('all deps loaded in order');
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+// });
+// });
+// });
+// });
