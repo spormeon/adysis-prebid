503f0e2bdb239677bae8e59cffa1fcb5146d7e0d
david stanton <davidstanton@spormeon.com>
2019-12-09 17:06:06 +0000

    no message

--- prebid/pub000002/lib/gpt.js
+++ prebid/pub000002/lib/gpt.js
@@ -2,9 +2,8 @@
 (function () {
 var gads = document.createElement("script");
 gads.async = true;
-gads.rel = "preload"
 gads.type = "text/javascript";
 gads.src = "https://securepubads.g.doubleclick.net/tag/js/gpt.js";
 var nodegads = document.getElementsByTagName("head")[0];
-nodegads.insertBefore(gads, nodegads.thirdChild);
+nodegads.insertBefore(gads, nodegads.firstChild);
 })();
