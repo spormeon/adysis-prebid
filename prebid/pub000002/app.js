e1864bcbbe5e2e816b44eb9a23a856172fb32e6f
david stanton <davidstanton@spormeon.com>
2019-12-09 20:46:12 +0000

    no message

--- prebid/pub000002/app.js
+++ prebid/pub000002/app.js
@@ -6,7 +6,7 @@ paths: { app: '../app' }
 //require(["app/slotsonpage"], function (slotsonpage) {
 require(["prebid2.40.0adyjs"], function (prebid) {
 require(["gpt"], function (gpt) {
-require(["bidfilter"], function (bidfilter) {
+require(["bidfilter_min"], function (bidfilter) {
 require(["app/timeoutmap_min"], function (timeoutmap) {
 require(["app/floorpricemap_min"], function (floorpricemap) {
 require(["app/siteconfig_min"], function (siteconfig) {
