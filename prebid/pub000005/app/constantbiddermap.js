f009c24ba9662efd3773cb29ae55a5fabcd0d729
david stanton <davidstanton@spormeon.com>
2020-03-10 09:44:09 +0000

    no message

---
+++ prebid/pub000005/app/constantbiddermap.js
@@ -0,0 +1,30 @@
+//FLOOR_PRICE MAP - 1000 def brings back bigger bids in Uk
+var constantbidderpriceMap = {
+0 : 0.20,
+1 : 0.21,
+2 : 0.22,
+3 : 0.23,
+4 : 0.24,
+5 : 0.25,
+6 : 0.06,
+7 : 0.07,
+8 : 0.08,
+9 : 0.06,
+10 : 0.01,
+11 : 0.01,
+12 : 0.02,
+13 : 0.03,
+14 : 0.04,
+15 : 0.05,
+16 : 0.06,
+17 : 0.07,
+18 : 0.08,
+19 : 0.09,
+20 : 0.01,
+21 : 0.01,
+22 : 0.02,
+23 : 0.03
+};
+var cbp = new Date().getUTCHours();
+CONSTANT_BIDDER_PRICE = constantbidderpriceeMap[cbp];
+console.log("Constant Bidder price:", CONSTANT_BIDDER_PRICE );
--- prebid/pub000005/app/prebidconfig.js
+++ prebid/pub000005/app/prebidconfig.js
@@ -66,7 +66,7 @@ emx_digital:   { bidCpmAdjustment : function(bidCpm){ if(bidCpm < FLOOR_PRICE){ 
 smartrtb:   { bidCpmAdjustment : function(bidCpm){ if(bidCpm < FLOOR_PRICE){ return 0;}return bidCpm * 1.00; } },
 viewdeosDX:   { bidCpmAdjustment : function(bidCpm){ if(bidCpm < FLOOR_PRICE){ return 0;}return bidCpm * 0.75; } },
 //adysis: { bidCpmAdjustment : function(bidCpm){ return "+c.cpm+" * 2;} },
-adysis: { bidCpmAdjustment : function(bidCpm){ return 0.28;} }
+adysis: { bidCpmAdjustment : function(bidCpm){ return CONSTANT_BIDDER_PRICE;} }
 //adysis: { bidCpmAdjustment : function(bidCpm){ if(bidCpm < 300 ){ return 0.68; } },
 };
     adyjs.setConfig({
