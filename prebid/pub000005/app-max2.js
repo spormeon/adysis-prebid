7b156f23908923a57749ccb6269977f39d660f7c
david stanton <davidstanton@spormeon.com>
2020-03-11 01:10:17 +0000

    no message

---
+++ prebid/pub000005/app-max2.js
@@ -0,0 +1,44 @@
+importScripts('lib/prebid3.10.0adyjs.js');
+// your application logic in there.
+//require(["app/slotsonpage"], function (slotsonpage) {
+//require(["lib/prebid2.40.0adyjs"], function (prebid) {
+require(["lib/gpt_min"], function (gpt) {
+require(["lib/bidfilter_min"], function (bidfilter) {
+require(["app/timeoutmap_min"], function (timeoutmap) {
+require(["app/floorpricemap_min"], function (floorpricemap) {
+require(["app/siteconfig_min"], function (siteconfig) {
+// require(["app/unrulyconfig"], function (unrulyconfig) {
+//require(["app/amazonbidder"], function (amazonbidder) {
+require(["app/bidcachemap_min"], function (bidcachemap) {
+require(["app/assertiveconfig_min"], function (assertiveconfig) {
+require(["app/nativebidders"], function (nativebidders) {
+require(["app/outstreambidders"], function (outstreambidders) {
+require(["app/bannerbidders"], function (bannerbidders) {
+require(["app/adunitsconfig"], function (adunitsconfig) {
+require(["app/custombuckets_min"], function (custombuckets) {
+require(["app/prebidconfig_min"], function (prebidconfig) {
+require(["app/assertivecustom_min"], function (assertivecustom) {
+require(["lib/assertive"], function (assertive) {
+require(["lib/preconnect_min"], function (preconnect) {	
+//require(["app/addata"], function (preconnect) {	
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+});
+//});
+//});
+// });
+// });
+// });
