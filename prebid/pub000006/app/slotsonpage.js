09eeaa80c31482ac5fadb3f5dcdb66f9e272c927
david stanton <davidstanton@spormeon.com>
2020-04-07 08:14:46 +0100

    no message

--- prebid/pub000006/app/slotsonpage.js
+++ prebid/pub000006/app/slotsonpage.js
@@ -1,3 +1,4 @@
+// load GPT tag
 (function(){var gads=document.createElement("script");gads.async=!0;gads.type="text/javascript";gads.src="https://securepubads.g.doubleclick.net/tag/js/gpt.js";var nodegads=document.getElementsByTagName("head")[0];nodegads.insertBefore(gads,nodegads.firstChild)})()
 //shows the slots on the page
 window.addEventListener("DOMContentLoaded",function(){
