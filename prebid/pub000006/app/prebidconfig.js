c65384865d43db685e5b4d7048ea7ae73067fd3f
david stanton <davidstanton@spormeon.com>
2020-04-08 17:10:59 +0100

    no message

--- prebid/pub000006/app/prebidconfig.js
+++ prebid/pub000006/app/prebidconfig.js
@@ -168,8 +168,8 @@ inreedvid5Slot: { adUnitPath: "/21665095471/specktra_halfpage", size: [[300, 600
 inreedvid8Slot: { adUnitPath: "/21665095471/specktra_threads_leaderboard_1", size: [[550, 310],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
 inreedvid4Slot: { adUnitPath: "/21665095471/specktra_threads_leaderboard_2", size: [[550, 310],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
 inreedvid7Slot: { adUnitPath: "/21665095471/specktra_threads_leaderboard_4", size: [[550, 310],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
-inreedvid10Slot: { adUnitPath: "/21665095471/specktra_footer_1", size: [[550, 310],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
-inreedvid6Slot: { adUnitPath: "/21665095471/specktra_footer_2", size: [[550, 310],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappingfooterslot", timeout: site_config.refresh_rate, },
+inreedvid10Slot: { adUnitPath: "/21665095471/specktra_footer_1", size: [[550, 310],[970, 250],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
+inreedvid6Slot: { adUnitPath: "/21665095471/specktra_footer_2", size: [[970, 90],[728, 90],[468, 60],[320, 50],[320, 100]], sizeMapping: "mappingfooterslot", timeout: site_config.refresh_rate, },
 },
 sizeMappings: {
 mappinginreedvidslot: [
