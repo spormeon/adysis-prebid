abbd190a82a633120a3d2c5e1626c60f63fd5e58
david stanton <davidstanton@spormeon.com>
2020-04-12 14:01:36 +0100

    no message

---
+++ prebid/pub000006/app/slotsonpage1.js
@@ -0,0 +1,37 @@
+//shows the slots on the page
+window.addEventListener("DOMContentLoaded", function() {
+  const ads = [
+    {
+      "selector": ".p-body-inner",
+      "html": '<div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid13Slot"></div></div><br>',
+    },
+    // This selector doesn't exist in this snippet and will be skipped by the condition below without crashing...
+    {
+      "selector": "#spacer1",
+      "html": '<div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid9Slot"></div></div><br><br><br>',
+    },
+    {
+      "selector": ".block.block--category.block--category99",
+      "html": '<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid8Slot"></div></div>'
+    },
+    {
+      "selector": "#spacer2",
+      "html": '<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid5Slot"></div></div><br>'
+    },
+    {
+      "selector": ".p-breadcrumbs.p-breadcrumbs--bottom",
+      "html": '<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid4Slot"></div></div><br>'
+    },
+    {
+      "selector": "#adfooter",
+      "html": '<br><div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid6Slot"></div></div><br>'
+    }
+    // Add your other ads here using similar structure above...
+  ];
+  ads.forEach(ad => {
+    adContainer = document.querySelector(ad.selector);
+    if (adContainer) {
+      adContainer.insertAdjacentHTML('afterbegin', ad.html);
+    }
+  });
+})
