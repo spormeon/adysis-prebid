4f1c70ab3e6ea739e602eed66393f8462dab7cb5
david stanton <davidstanton@spormeon.com>
2020-04-16 13:21:56 +0100

    no message

--- prebid/pub000006/app/prebidconfig.js
+++ prebid/pub000006/app/prebidconfig.js
@@ -150,7 +150,7 @@ googletag.cmd.push(function () {
     //googletag.pubads().disableInitialLoad();
     googletag.enableServices();
 // not sure if impressionViewable, slotRenderEnded or slotOnload or impressionViewable is best to use yet
-    googletag.pubads().addEventListener("slotRenderEnded", function (event) {
+    googletag.pubads().addEventListener("impressionViewable", function (event) {
      var elementId = event.slot.getSlotElementId();
       var slotConfig = slots[elementId];
        if (slotConfig) {
