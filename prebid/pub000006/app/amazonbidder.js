bd2793e5f6153a7890b571d03e44f09cbaa37b26
david stanton <davidstanton@spormeon.com>
2020-04-20 14:48:06 +0100

    no message

--- prebid/pub000006/app/amazonbidder.js
+++ prebid/pub000006/app/amazonbidder.js
@@ -1,21 +1,19 @@
 // amazon bidder
 !function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
 apstag.init({
-   pubID: '7f2180c4-57bc-43a4-9896-ecff1a52049d',
+   pubID: '853ec078-af51-43d8-938b-4b02c2a8c260',
    adServer: 'googletag'
  });
 apstag.fetchBids({
 slots: [
-    { slotID: 'inreedvid4Slot', slotName: '/1001824/adp100001/adp100001A', sizes: [[970, 250], [970, 90], [728, 90], [468, 60], [300, 250], [250, 250], [320, 50]] },
-    { slotID: 'inreedvid5Slot', slotName: '/1001824/adp100001/adp100001B', sizes: [[728, 90], [300, 250], [250, 250], [468, 60], [320, 50]] },
-    { slotID: 'inreedvid6Slot', slotName: '/1001824/adp100001/adp100001C', sizes: [[728, 90], [300, 250], [250, 250], [468, 60], [320, 50]] },
-    { slotID: 'inreedvid7Slot', slotName: '/1001824/adp100001/adp100001D', sizes: [[728, 90], [300, 250], [250, 250], [468, 60], [320, 50]] },
-    { slotID: 'inreedvid8Slot', slotName: '/1001824/adp100001/adp100001E', sizes: [[970, 250], [970, 90], [728, 90], [468, 60], [300, 250], [250, 250], [320, 50]] },
-    { slotID: 'inreedvid9Slot', slotName: '/1001824/adp100001/adp100001F', sizes: [[300, 600], [300, 250], [250, 250]] },
-    { slotID: 'inreedvid10Slot', slotName: '/1001824/adp100001/adp100001G', sizes: [[300, 600], [300, 250], [250, 250]] },
-    { slotID: 'inreedvid11Slot', slotName: '/1001824/adp100001/adp100001H', sizes: [[300, 600], [160, 600], [120, 600]] },
-    { slotID: 'inreedvid12Slot', slotName: '/1001824/adp100001/adp100001I', sizes: [[300, 600], [160, 600], [120, 600]] },
-    { slotID: 'inreedvid13Slot', slotName: '/1001824/adp100001/adp100001J', sizes: [[970, 250], [970, 90], [728, 90], [468, 60], [300, 250], [250, 250], [320, 50]] }
+    { slotID: 'inreedvid4Slot', slotName: '/21665095471/specktra_threads_leaderboard_2', sizes: [[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]] },
+    { slotID: 'inreedvid5Slot', slotName: '/21665095471/specktra_halfpage', sizes: [[300, 600],[300, 250],[250, 250],[160, 600],[120, 600]] },
+    { slotID: 'inreedvid6Slot', slotName: '/21665095471/specktra_footer_2', sizes: [[970, 90],[728, 90],[468, 60],[320, 50],[320, 100]] },
+    { slotID: 'inreedvid7Slot', slotName: '/21665095471/specktra_threads_leaderboard_4', sizes: [[970, 250],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]] },
+    { slotID: 'inreedvid8Slot', slotName: '/21665095471/specktra_threads_leaderboard_1', sizes: [[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]] },
+    { slotID: 'inreedvid9Slot', slotName: '/21665095471/specktra_mrec_1', sizes: [[300, 600],[300, 250],[250, 250],[160, 600],[120, 600]] },
+    { slotID: 'inreedvid10Slot', slotName: '/21665095471/specktra_footer_1', sizes: [[970, 250],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]] },
+    { slotID: 'inreedvid13Slot', slotName: '/21665095471/specktra_top_1', sizes: [[970, 90],[970, 250],[728, 90],[468, 60],[320, 50],[320, 100]] }
     ],
     timeout: 1e3
  }, function(bids) {
