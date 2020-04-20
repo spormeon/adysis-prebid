9e722bbf0fdeabd0ca23e1b4d90a3e0999c6b06d
david stanton <davidstanton@spormeon.com>
2020-04-20 15:07:54 +0100

    no message

--- prebid/pub000006/app/amazonbidder.js
+++ prebid/pub000006/app/amazonbidder.js
@@ -1,7 +1,7 @@
 // amazon bidder
 !function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
 apstag.init({
-   pubID: '853ec078-af51-43d8-938b-4b02c2a8c260',
+   pubID: '7f2180c4-57bc-43a4-9896-ecff1a52049d',
    adServer: 'googletag'
  });
 apstag.fetchBids({
@@ -24,3 +24,7 @@ slots: [
      });
  });
 // amazon bidder end
+
+
+
+
