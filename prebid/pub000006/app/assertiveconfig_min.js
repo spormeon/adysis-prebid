fb107149e51cfefebba9d001c76d40c4760627a6
david stanton <davidstanton@spormeon.com>
2020-04-20 20:37:22 +0100

    no message

--- prebid/pub000006/app/assertiveconfig_min.js
+++ prebid/pub000006/app/assertiveconfig_min.js
@@ -1,1 +1,2 @@
-var assertive_entityId="FHkAkgiDWrXm4dZFw";var assertive_debug=0;var assertive_sampleRate=1;var assertive_timeout=null;var assertive_layout=null;var assertive_userState=null;var assertive_custom_2=null;var assertive_custom_3=null;var assertive_custom_4=null;var assertive_custom_5=null;assertive_timeout=PREBID_TIMEOUT
+
+var assertive_entityId="ZeGTvRTjAGdsh4k5c";var assertive_debug=0;var assertive_sampleRate=1;var assertive_timeout=null;var assertive_layout=null;var assertive_userState=null;var assertive_custom_2=null;var assertive_custom_3=null;var assertive_custom_4=null;var assertive_custom_5=null;assertive_timeout=PREBID_TIMEOUT
