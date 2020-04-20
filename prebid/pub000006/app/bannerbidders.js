1c5616418c50725b1284db361d5bda4d2dbc3013
david stanton <davidstanton@spormeon.com>
2020-04-20 09:14:14 +0100

    no message

--- prebid/pub000006/app/bannerbidders.js
+++ prebid/pub000006/app/bannerbidders.js
@@ -1,15 +1,12 @@
 // BANNER BIDDER LIST
 var banner_bidders = {
 bids: [
-//{ bidder: 'teads', params: { placementId: '75853', pageId: '87372' } },
 { bidder: 'appnexus', params: { placementId: '14103198', allowSmallerSizes: true } }, /* one placementId for all sizes */
 { bidder: 'sovrn', params: { tagid: '590821' } }, /* 728x90 970x90 970x250 */
-{ bidder: 'sovrn', params: { tagid: '662394' } }, /* 160x600 300x600 */
 { bidder: 'sovrn', params: { tagid: '590819' } }, /* 320x50 */
 { bidder: 'sovrn', params: { tagid: '662395' } }, /* 250x250 300x250 */
 { bidder: 'openx', params: { unit: '540375630', delDomain: 'huddler-d.openx.net' } }, /* 728x90 970x90 970x250 */
 { bidder: 'openx', params: { unit: '540375636', delDomain: 'huddler-d.openx.net' } }, /* 320x50 250x250 300x250 */
-{ bidder: 'openx', params: { unit: '540923378', delDomain: 'huddler-d.openx.net' } }, /* 160x600 300x600 */
 { bidder: 'sharethrough', params: { pkey: "esV9zAvAVRANpBbComRYEUWv", iframe: false } } /* Sizeless */
 ] //./bids
 };
