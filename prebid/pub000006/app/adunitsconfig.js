62d65af32fd8fb7e0dc5c8043286006b118794bd
david stanton <davidstanton@spormeon.com>
2020-04-21 13:44:16 +0100

    no message

--- prebid/pub000006/app/adunitsconfig.js
+++ prebid/pub000006/app/adunitsconfig.js
@@ -11,7 +11,7 @@ bids: native_bidders.bids
 code: 'inreedvid4Slot',
 labelAny: ['desktop', 'tablet', 'phone'],
 mediaTypes: { video:  { context: 'outstream', playerSize: [[300, 250]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2], minduration: 0,  maxduration: 45, protocols: [1,2,3,4,5,6,7,8,9,10], linearity: 1, api: [1,2,3,4,5,6] } },
-renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCode, adResponse: bid.adResponse, }); } },
+renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCodes, adResponse: bid.adResponse, }); } },
 renderer: { options: { adText: 'Advertisement' } },
 bids: outstream_bidders.bids
 },
@@ -33,7 +33,7 @@ bids: native_bidders.bids
 code: 'inreedvid5Slot',
 labelAny: ['desktopmenu1', 'tablet', 'phone'],
 mediaTypes: { video:  { context: 'outstream', playerSize: [[300, 250]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2], minduration: 0,  maxduration: 45, protocols: [1,2,3,4,5,6,7,8,9,10], linearity: 1, api: [1,2,3,4,5,6] } },
-renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCode, adResponse: bid.adResponse, }); } },
+renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCodes, adResponse: bid.adResponse, }); } },
 renderer: { options: { adText: 'Advertisement' } },
 bids: outstream_bidders.bids
 },
@@ -63,7 +63,7 @@ bids: native_bidders.bids
 code: 'inreedvid7Slot',
 labelAny: ['desktopbottom2', 'tabletbottom2', 'phonebottom2'],
 mediaTypes: { video:  { context: 'outstream', playerSize: [[300, 250]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2], minduration: 0,  maxduration: 45, protocols: [1,2,3,4,5,6,7,8,9,10], linearity: 1, api: [1,2,3,4,5,6] } },
-renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCode, adResponse: bid.adResponse, }); } },
+renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCodes, adResponse: bid.adResponse, }); } },
 renderer: { options: { adText: 'Advertisement' } },
 bids: outstream_bidders.bids
 },
@@ -85,7 +85,7 @@ bids: native_bidders.bids
 code: 'inreedvid8Slot',
 labelAny: ['desktopmenu', 'tabletmenu', 'phonemenu'],
 mediaTypes: { video:  { context: 'outstream', playerSize: [[300, 250]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2], minduration: 0,  maxduration: 45, protocols: [1,2,3,4,5,6,7,8,9,10], linearity: 1, api: [1,2,3,4,5,6] } },
-renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCode, adResponse: bid.adResponse, }); } },
+renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCodes, adResponse: bid.adResponse, }); } },
 renderer: { options: { adText: 'Advertisement' } },
 bids: outstream_bidders.bids
 },
@@ -107,7 +107,7 @@ bids: native_bidders.bids
 code: 'inreedvid9Slot',
 labelAny: ['desktopmenu', 'tabletmenu', 'phonemenu'],
 mediaTypes: { video:  { context: 'outstream', playerSize: [[300, 250]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2], minduration: 0,  maxduration: 45, protocols: [1,2,3,4,5,6,7,8,9,10], linearity: 1, api: [1,2,3,4,5,6] } },
-renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCode, adResponse: bid.adResponse, }); } },
+renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCodes, adResponse: bid.adResponse, }); } },
 renderer: { options: { adText: 'Advertisement' } },
 bids: outstream_bidders.bids
 },
@@ -129,7 +129,7 @@ bids: native_bidders.bids
 code: 'inreedvid10Slot',
 labelAny: ['desktopbottom2', 'tabletbottom2', 'phonebottom2'],
 mediaTypes: { video:  { context: 'outstream', playerSize: [[300, 250]], mimes: ['video/x-flv', 'video/mp4', 'application/x-shockwave-flash', 'application/javascript', 'video/webm'], playbackmethod: [2], minduration: 0,  maxduration: 45, protocols: [1,2,3,4,5,6,7,8,9,10], linearity: 1, api: [1,2,3,4,5,6] } },
-renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCode, adResponse: bid.adResponse, }); } },
+renderer: { url: 'https://cdn.jsdelivr.net/gh/spormeon/adysis-prebid/prebid/ANOutstreamVideo.js', render: function (bid) { ANOutstreamVideo.renderAd({ targetId: bid.adUnitCodes, adResponse: bid.adResponse, }); } },
 renderer: { options: { adText: 'Advertisement' } },
 bids: outstream_bidders.bids
 },
@@ -148,3 +148,11 @@ mediaTypes: { banner: { sizes: [[970, 90],[970, 250],[728, 90],[468, 60],[320, 5
 bids: banner_bidders.bids
 }
 ];
+
+
+
+
+
+
+
+
