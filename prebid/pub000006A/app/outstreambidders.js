5dcd589bdeaa809496d318d1a23eed048fb815fd
david stanton <davidstanton@spormeon.com>
2020-04-23 10:53:57 +0100

    no message

---
+++ prebid/pub000006A/app/outstreambidders.js
@@ -0,0 +1,10 @@
+//OUTSTREAM BIDDER LIST
+var outstream_bidders = {
+bids: [
+{ bidder: 'appnexus', params: { placementId: '14103198', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_off'] } } }, /* one placementId for all sizes */
+//{ bidder: 'appnexus', params: { placementId: '13232385', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_off'] } } }, /* APPNEXUS TEST ID one placementId for all sizes */	
+] //./bids
+};
+
+
+
