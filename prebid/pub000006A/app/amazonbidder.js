1f94f5a70c3ad2b6effe3aebb632a6de53162b5f
david stanton <davidstanton@spormeon.com>
2020-04-23 11:11:23 +0100

    no message

--- prebid/pub000006A/app/amazonbidder.js
+++ prebid/pub000006A/app/amazonbidder.js
@@ -24,3 +24,29 @@ slots: [
      });
  });
 // amazon bidder end
+
+
+//call refreshBids() to make an UAM request for all slots in apstagSlots and then call googletag to refresh all
+//defined slots on the page
+function refreshBids() {
+  apstag.fetchBids({
+      slots: [ 
+    	  { slotID: 'inreedvid4Slot', slotName: '/21665095471/specktra_threads_leaderboard_2', sizes: [[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]] },
+    	  { slotID: 'inreedvid5Slot', slotName: '/21665095471/specktra_halfpage', sizes: [[300, 600],[300, 250],[250, 250],[160, 600],[120, 600]] },
+    	  { slotID: 'inreedvid6Slot', slotName: '/21665095471/specktra_footer_2', sizes: [[970, 90],[728, 90],[468, 60],[320, 50],[320, 100]] },
+    	  { slotID: 'inreedvid7Slot', slotName: '/21665095471/specktra_threads_leaderboard_4', sizes: [[970, 250],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]] },
+    	  { slotID: 'inreedvid8Slot', slotName: '/21665095471/specktra_threads_leaderboard_1', sizes: [[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]] },
+    	  { slotID: 'inreedvid9Slot', slotName: '/21665095471/specktra_mrec_1', sizes: [[300, 600],[300, 250],[250, 250],[160, 600],[120, 600]] },
+    	  { slotID: 'inreedvid10Slot', slotName: '/21665095471/specktra_footer_1', sizes: [[970, 250],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]] },
+    	  { slotID: 'inreedvid13Slot', slotName: '/21665095471/specktra_top_1', sizes: [[970, 90],[970, 250],[728, 90],[468, 60],[320, 50],[320, 100]] }
+      ],
+      timeout: 1e3
+  }, function(bids) {
+      // set apstag targeting on googletag 
+      //then refresh all DFP
+      googletag.cmd.push(function() {
+          apstag.setDisplayBids();
+          googletag.pubads().refresh();
+      });
+  });
+}
