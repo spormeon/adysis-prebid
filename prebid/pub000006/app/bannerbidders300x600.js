746a2ca7f21396aad83c6660f68e87db4543f290
david stanton <davidstanton@spormeon.com>
2020-04-24 07:27:36 +0100

    no message

--- prebid/pub000006/app/bannerbidders300x600.js
+++ prebid/pub000006/app/bannerbidders300x600.js
@@ -6,6 +6,6 @@ bids: [
 { bidder: 'sovrn', params: { tagid: '662395' } }, /* 250x250 300x250 */
 { bidder: 'openx', params: { unit: '540375629', delDomain: 'huddler-d.openx.net' } }, /* 300x250 */
 { bidder: 'openx', params: { unit: '540923378', delDomain: 'huddler-d.openx.net' } }, /* 160x600 300x600 */
-{ bidder: 'sharethrough', params: { pkey: "esV9zAvAVRANpBbComRYEUWv", iframe: true } } /* Sizeless */
+{ bidder: 'sharethrough', params: { pkey: "esV9zAvAVRANpBbComRYEUWv", iframe: false } } /* Sizeless */
 ] //./bids
 };
