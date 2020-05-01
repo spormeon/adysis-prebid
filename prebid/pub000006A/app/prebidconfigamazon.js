5d313b37137e0d3a30cad641f637c39ea070d311
david stanton <davidstanton@spormeon.com>
2020-05-01 11:38:29 +0100

    no message

--- prebid/pub000006A/app/prebidconfigamazon.js
+++ prebid/pub000006A/app/prebidconfigamazon.js
@@ -72,16 +72,16 @@ pbjs.que.push(function() {
             { mediaQuery: '(min-width: 500px) and (max-width: 768px)', sizesSupported: [[728,90],[468,60],[320,50]], labels: [ 'tabletleader'] },
             { mediaQuery: '(min-width: 1px) and (max-width: 499px)', sizesSupported: [[300,250],[250,250],[320,50],[320,100],[336,280]], labels: [ 'phone'] },
             { mediaQuery: '(min-width: 1px) and (max-width: 499px)', sizesSupported: [[468,60],[320,50]], labels: [ 'phonefooter'] },
-            { mediaQuery: '(min-width: 1px) and (max-width: 499px)', sizesSupported: [[300, 250],[250, 250],[320, 50],[320, 100],[336, 280],[468, 90]], labels: [ 'phonebottom2'] },
+            { mediaQuery: '(min-width: 1px) and (max-width: 499px)', sizesSupported: [[300, 250],[250, 250],[320, 50],[320, 100],[336, 280],[468, 60]], labels: [ 'phonebottom2'] },
             { mediaQuery: '(min-width: 1px) and (max-width: 499px)', sizesSupported: [[468,60],[320,50]], labels: [ 'phoneleader'] },
             { mediaQuery: '(min-width: 1px) and (max-width: 499px)', sizesSupported: [[300,250],[250,250]], labels: [ 'phonemenu'] }
           ]
     });
     
-    pbjs.requestBids({
-        bidsBackHandler: biddersBack,
-        timeout: PREBID_TIMEOUT
-    });
+    // pbjs.requestBids({
+    //    bidsBackHandler: biddersBack,
+    //    timeout: PREBID_TIMEOUT
+    // });
 });
 
 googletag.cmd.push(function() {
@@ -170,7 +170,7 @@ sizeMappings: {
 	mappinginreedvidslot: [
 	[[1024, 768],[[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]]],
 	[[768, 500],[[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]]],
-	[[1, 1],[[300, 250],[250, 250],[320, 50],[320, 100],[336, 280],[468, 90]]]
+	[[1, 1],[[300, 250],[250, 250],[320, 50],[320, 100],[336, 280],[468, 60]]]
 	],
 	mappingmenuslot: [
 	[[1024, 768],[[300, 600],[300, 250],[250, 250],[160, 600],[120, 600]]],
@@ -185,7 +185,7 @@ sizeMappings: {
 	mappingbottom2slot: [
 	[[1024, 768],[[970, 250],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]]],
 	[[768, 500],[[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]]],
-	[[1, 1],[[300, 250],[250, 250],[320, 50],[320, 100],[336, 280],[468, 90]]]
+	[[1, 1],[[300, 250],[250, 250],[320, 50],[320, 100],[336, 280],[468, 60]]]
 	],
 	mappingfooterslot: [
 	[[1024, 768],[[970, 90],[728, 90],[468, 60],[320, 50],[320, 100]]],
