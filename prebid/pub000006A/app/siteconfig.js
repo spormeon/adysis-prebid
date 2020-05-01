28b7ba45f241b98b68568c2e8625640f8cb84c8e
david stanton <davidstanton@spormeon.com>
2020-05-01 10:13:16 +0100

    no message

--- prebid/pub000006A/app/siteconfig.js
+++ prebid/pub000006A/app/siteconfig.js
@@ -1,9 +1,14 @@
 var site_config = {
-refresh_rate: PREBID_TIMEOUT*30;  //denoted in milliseonds 40secs=40000...
-FAILSAFE_TIMEOUT: PREBID_TIMEOUT*2;   //denoted in milliseonds 5secs=5000...
-LAZYLOAD_FETCH: 20;    // Fetch slots within X viewports
-LAZYLOAD_RENDER: 15;     // Render slots within X viewports.
-LAZYLOAD_MOBILE_SCALE: 1.3  // Scale the above values on mobile.
+refresh_rate: PREBID_TIMEOUT*30,  
+//denoted in milliseonds 40secs=40000...
+FAILSAFE_TIMEOUT: PREBID_TIMEOUT*2,   
+//denoted in milliseonds 5secs=5000...
+LAZYLOAD_FETCH: 20,    
+// Fetch slots within X viewports
+LAZYLOAD_RENDER: 15,     
+// Render slots within X viewports.
+LAZYLOAD_MOBILE_SCALE: 1.3  
+// Scale the above values on mobile.
 };
 // site_config end
 console.log("Lazy Load Fetch:", LAZYLOAD_FETCH );
