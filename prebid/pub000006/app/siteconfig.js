64e7a76ba019df4822bd1a9b3e0dd7d3962316de
david stanton <davidstanton@spormeon.com>
2020-05-05 12:34:09 +0100

    25 fetch 20 render 1.2 mobile LL

--- prebid/pub000006/app/siteconfig.js
+++ prebid/pub000006/app/siteconfig.js
@@ -1,12 +1,14 @@
 var site_config = {
 refresh_rate: 30000,         // denoted in milliseonds 40secs=40000...
 FAILSAFE_TIMEOUT: PREBID_TIMEOUT*2,      // denoted in milliseonds 5secs=5000...
-LAZYLOAD_FETCH: 20,                      // Fetch slots within X viewports
-LAZYLOAD_RENDER: 15,                     // Render slots within X viewports.
-LAZYLOAD_MOBILE_SCALE: 1.3               // Scale the above values on mobile.
+LAZYLOAD_FETCH: 25,                      // Fetch slots within X viewports
+LAZYLOAD_RENDER: 20,                     // Render slots within X viewports.
+LAZYLOAD_MOBILE_SCALE: 1.2               // Scale the above values on mobile.
 };
 // site_config end
 console.log('Lazy Load Fetch = ' +site_config.LAZYLOAD_FETCH );
 console.log('Lazy Load Render = ' +site_config.LAZYLOAD_RENDER );
 console.log('Lazy Load Mobile Scale = ' +site_config.LAZYLOAD_MOBILE_SCALE );
+
+
 
