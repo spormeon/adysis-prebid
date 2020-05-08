f0ec0af4c0fc45b0972d467655f8379a27c1a653
david stanton <davidstanton@spormeon.com>
2020-05-08 13:36:05 +0100

    no message

--- prebid/pub000011/app/slotsonpage.js
+++ prebid/pub000011/app/slotsonpage.js
@@ -1,7 +1,7 @@
 //shows the slots on the page
 window.addEventListener("DOMContentLoaded", function() {
  const ads = [
-{ "selector": ".fusion-header-tagline.fusion-responsive-typography-calculated", "location": "afterbegin", "html": '<div class="ad-reporter-ahytrfg35423"><div id="inreedvid13Slot"></div></div>' },
+{ "selector": ".fusion-header-content-3-wrapper", "location": "afterbegin", "html": '<div class="ad-reporter-ahytrfg35423"><div id="inreedvid13Slot"></div></div>' },
 { "selector": "#custom_html-23", "location": "afterbegin", "html": '<div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid9Slot"></div></div><br>' },
 { "selector": "#adrotate_widgets-59", "location": "beforebegin", "html": '<div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid10Slot"></div></div><br>' },
 { "selector": "#custom_html-22", "location": "beforeend", "html": '<div class="ad-reporter-ahytrfg35423"><div id="advertisement" style="border: 0pt none; margin: auto; text-align: center; color: #999; text-transform: uppercase; font-family: sans-serif; font-size: 9px; font-weight: 400; letter-spacing: .2em; line-height: 1; margin-top: 0px; position: relative; top: -4px;">Advertisement</div><div id="inreedvid8Slot"></div></div>' },
@@ -22,4 +22,11 @@ window.addEventListener("DOMContentLoaded", function() {
    }
  });
 });
+
+
+
+
+
+
+
 
