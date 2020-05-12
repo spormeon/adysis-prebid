e792760d5c749f76c1ed788e3e3ae95ae319d92e
david stanton <davidstanton@spormeon.com>
2020-05-12 17:00:50 +0100

    no message

--- prebid/pub000011/app/adunitsconfig.js
+++ prebid/pub000011/app/adunitsconfig.js
@@ -48,7 +48,7 @@ bids: banner_bidders300x600.bids
 {
 code: 'inreedvid6Slot',
 labelAny: ['desktopfooter', 'tabletfooter', 'phonefooter'],
-mediaTypes: { banner: { sizes: [[970,90],[728,90],[468,60],[320,50]] } },
+mediaTypes: { banner: { sizes: [[970,90],[728,90],[468,60],[320,50],[1,1]] } },
 bids: banner_bidders.bids
 },
 //new ad unit block
@@ -140,6 +140,18 @@ mediaTypes: { banner: { sizes: [[728, 90],[468, 60],[320, 50],[320, 100]] } },
 bids: banner_bidders.bids
 }
 ];
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- prebid/pub000011/app/prebidconfigamazon.js
+++ prebid/pub000011/app/prebidconfigamazon.js
@@ -180,7 +180,7 @@ inreedvid8Slot: { adUnitPath: "/21665095471/sm_skyscraper_1", size: [[160, 600],
 inreedvid4Slot: { adUnitPath: "/21665095471/sm_leaderboard_1", size: [[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
 inreedvid7Slot: { adUnitPath: "/21665095471/sm_leaderboard_2", size: [[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
 // inreedvid10Slot: { adUnitPath: "/21665095471/SMF_Footer_1", size: [[970, 250],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappingbottom2slot", timeout: site_config.refresh_rate, },
-inreedvid6Slot: { adUnitPath: "/21665095471/sm_footer_1", size: [[970, 90],[728, 90],[468, 60],[320, 50],[320, 100]], sizeMapping: "mappingfooterslot", timeout: site_config.refresh_rate, },
+inreedvid6Slot: { adUnitPath: "/21665095471/sm_footer_1", size: [[970, 90],[728, 90],[468, 60],[320, 50],[320, 100],[1,1]], sizeMapping: "mappingfooterslot", timeout: site_config.refresh_rate, },
 },
 sizeMappings: {
 	mappinginreedvidslot: [
@@ -209,8 +209,8 @@ sizeMappings: {
 	[[1, 1],[[300, 250],[250, 250],[320, 50],[320, 100],[336, 280],[468, 60]]]
 	],
 	mappingfooterslot: [
-	[[1024, 768],[[970, 90],[728, 90],[468, 60],[320, 50],[320, 100]]],
-	[[768, 500],[[728, 90],[468, 60],[320, 50]]],
+	[[1024, 768],[[970, 90],[728, 90],[468, 60],[320, 50],[320, 100],[1,1]]],
+	[[768, 500],[[728, 90],[468, 60],[320, 50],[1,1]]],
 	[[1, 1],[[468, 60],[320, 50]]]
 	],
 	mappingleaderslot: [
