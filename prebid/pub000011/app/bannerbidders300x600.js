b5fb48f6169b99ccfa3c824f619acf0239df37a9
david stanton <davidstanton@spormeon.com>
2020-05-12 15:40:11 +0100

    no message

--- prebid/pub000011/app/bannerbidders300x600.js
+++ prebid/pub000011/app/bannerbidders300x600.js
@@ -2,26 +2,19 @@
 var banner_bidders300x600 = {
 bids: [
 { bidder: 'appnexus', params: { placementId: '13211644', allowSmallerSizes: true } }, /* one placementId for all sizes */
+{ bidder: 'districtm', params: { placementId: '13445865', allowSmallerSizes: true } }, /* one placementId for all sizes */
+{ bidder: 'districtmDMX', params: { dmxid:'221835', memberid:'101399' } },
+{ bidder: 'gourmetads', params: { placementId:'13192578' allowSmallerSizes: true } },
+{ bidder: 'conversant', params: { site_id:'117478', secure:1 } },
 { bidder: 'ix', params: { siteId:'285984', size: [300, 600] } },
 { bidder: 'ix', params: { siteId:'285984', size: [160, 600] } },
 { bidder: 'ix', params: { siteId:'285980', size: [300, 250] } },
 { bidder: 'ix', params: { siteId:'285980', size: [250, 250] } },
-{ bidder: 'sovrn', params: { tagid: '662394' } }, /* 160x600 300x600 */
-{ bidder: 'sovrn', params: { tagid: '662395' } }, /* 250x250 300x250 */
-{ bidder: 'openx', params: { unit: '540375629', delDomain: 'huddler-d.openx.net' } }, /* 300x250 */
-{ bidder: 'openx', params: { unit: '540923378', delDomain: 'huddler-d.openx.net' } }, /* 160x600 300x600 */
+{ bidder: 'sovrn', params: { tagid: '590978' } }, /* 160x600 300x600 */
+{ bidder: 'sovrn', params: { tagid: '590980' } }, /* 250x250 300x250 */
+{ bidder: 'openx', params: { unit: '539995986', delDomain: 'huddler-d.openx.net' } }, /* 300x250 250x250 */
+{ bidder: 'openx', params: { unit: '539995988', delDomain: 'huddler-d.openx.net' } }, /* 160x600 300x600 */
 { bidder: 'sharethrough', params: { pkey: "auoW1g6UJHnw8XCAzrbTwfpV", iframe: true } } /* Sizeless */
 ] //./bids
 };
-
-
-
-
-
-
-
-
-
-
-
 
