890929e59bc7f6d80a4c7e2f76d0d99c6976e022
david stanton <davidstanton@spormeon.com>
2020-05-14 18:43:24 +0100

    no message

--- prebid/pub000006/app/bannerbidders.js
+++ prebid/pub000006/app/bannerbidders.js
@@ -7,14 +7,13 @@ bids: [
 { bidder: 'sovrn', params: { tagid: '662395' } }, /* 250x250 300x250 */
 { bidder: 'openx', params: { unit: '540375630', delDomain: 'huddler-d.openx.net' } }, /* 728x90 970x90 970x250 */
 { bidder: 'openx', params: { unit: '540375636', delDomain: 'huddler-d.openx.net' } }, /* 320x50 250x250 300x250 */
-{ bidder: 'sharethrough', params: { pkey: "esV9zAvAVRANpBbComRYEUWv", iframe: true } } /* Sizeless */
+{ bidder: 'sharethrough', params: { pkey: "esV9zAvAVRANpBbComRYEUWv", iframe: true } }, /* Sizeless */
+{ bidder: 'aol', params: { placement: '5219024', network: '11361.1' } }, /* 300x250 */ 
+{ bidder: 'aol', params: { placement: '5219022', network: '11361.1' } }, /* 250x250 */ 
+{ bidder: 'aol', params: { placement: '5219025', network: '11361.1' } }, /* 320x50 */ 
+{ bidder: 'aol', params: { placement: '5219023', network: '11361.1' } }, /* 336x280 */ 
+{ bidder: 'aol', params: { placement: '5219021', network: '11361.1' } }, /* 728x900 */ 
+{ bidder: 'aol', params: { placement: '5218988', network: '11361.1' } }, /* 970x90 */ 
+{ bidder: 'aol', params: { placement: '5219003', network: '11361.1' } }, /* 468x60 */ 
 ] //./bids
 };
-
-
-
-
-
-
-
-
--- prebid/pub000006/app/bannerbidders300x600.js
+++ prebid/pub000006/app/bannerbidders300x600.js
@@ -6,10 +6,10 @@ bids: [
 { bidder: 'sovrn', params: { tagid: '662395' } }, /* 250x250 300x250 */
 { bidder: 'openx', params: { unit: '540375629', delDomain: 'huddler-d.openx.net' } }, /* 300x250 */
 { bidder: 'openx', params: { unit: '540923378', delDomain: 'huddler-d.openx.net' } }, /* 160x600 300x600 */
-{ bidder: 'sharethrough', params: { pkey: "esV9zAvAVRANpBbComRYEUWv", iframe: true } } /* Sizeless */
+{ bidder: 'sharethrough', params: { pkey: "esV9zAvAVRANpBbComRYEUWv", iframe: true } }, /* Sizeless */
+{ bidder: 'aol', params: { placement: '5219024', network: '11361.1' } }, /* 300x250 */ 
+{ bidder: 'aol', params: { placement: '5219002', network: '11361.1' } }, /* 120x600 */ 
+{ bidder: 'aol', params: { placement: '5219004', network: '11361.1' } }, /* 160x600 */ 
+{ bidder: 'aol', params: { placement: '5219005', network: '11361.1' } } /* 300x600 */ 
 ] //./bids
 };
-
-
-
-
