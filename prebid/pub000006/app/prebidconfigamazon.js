1e561d186e9d194fe2f2ea38ad2e1619c835f637
david stanton <davidstanton@spormeon.com>
2020-05-14 16:00:26 +0100

    no message

--- prebid/pub000006/app/prebidconfigamazon.js
+++ prebid/pub000006/app/prebidconfigamazon.js
@@ -78,10 +78,10 @@ pbjs.que.push(function() {
           ]
     });
     
-    pbjs.requestBids({
-       bidsBackHandler: biddersBack,
-       timeout: PREBID_TIMEOUT
-    });
+    //pbjs.requestBids({
+    //  bidsBackHandler: biddersBack,
+    //   timeout: PREBID_TIMEOUT
+    //});
 });
 
 googletag.cmd.push(function() {
@@ -322,17 +322,5 @@ function requestHeaderBidsRefresh(slot) {
 window.setTimeout(function() {
     sendAdserverRequest();
 }, PREBID_TIMEOUT);
-
-
-
-
-
-
-
-
-
-
-
-
 
 
