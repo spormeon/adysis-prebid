8ab66a2a262b8266a8d0272882612a6b8a2c1f47
david stanton <davidstanton@spormeon.com>
2020-05-15 14:12:29 +0100

    no message

--- prebid/pub000009/app/bannerbidders.js
+++ prebid/pub000009/app/bannerbidders.js
@@ -10,7 +10,7 @@ bids: [
 { bidder: 'sovrn', params: { tagid: '725046' } }, /* 336x280 */
 { bidder: 'sovrn', params: { tagid: '725044' } }, /* 970x90  */
 { bidder: 'sovrn', params: { tagid: '725085' } }, /* 300x250 */
-{ bidder: 'sovrn', params: { tagid: '725045' } }, /* 468x60 */
+//{ bidder: 'sovrn', params: { tagid: '725045' } }, /* 468x60 */
 //{ bidder: 'sovrn', params: { tagid: '725043' } }, /* 970x250 */
 { bidder: 'openx', params: { unit: '540375630', delDomain: 'huddler-d.openx.net' } }, /* 728x90 970x90 970x250 */
 { bidder: 'openx', params: { unit: '540375636', delDomain: 'huddler-d.openx.net' } }, /* 320x50 250x250 300x250 */
@@ -24,9 +24,4 @@ bids: [
 { bidder: 'aol', params: { placement: '5219093', network: '11361.1' } } /* 468x60 */
 ] //./bids
 };
-
-
-
-
-
 
