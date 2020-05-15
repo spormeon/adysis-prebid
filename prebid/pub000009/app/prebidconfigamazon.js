f8785aa1ba5af11932ea98faa6664ca1ca08fe7f
david stanton <davidstanton@spormeon.com>
2020-05-15 12:03:25 +0100

    no message

--- prebid/pub000009/app/prebidconfigamazon.js
+++ prebid/pub000009/app/prebidconfigamazon.js
@@ -171,7 +171,7 @@ inreedvid9Slot: { adUnitPath: "/21665095471/SMF_Mrec_1", size: [[300, 600],[300,
 inreedvid5Slot: { adUnitPath: "/21665095471/SMF_Skyscraper_1", size: [[300, 600],[300, 250],[250, 250],[160, 600],[120, 600]], sizeMapping: "mappingmenuslot", timeout: site_config.refresh_rate, },
 inreedvid8Slot: { adUnitPath: "/21665095471/SMF_Threads_Leaderboard_2", size: [[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
 inreedvid4Slot: { adUnitPath: "/21665095471/SMF_Threads_Leaderboard_3", size: [[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
-inreedvid7Slot: { adUnitPath: "/21665095471/SMF_Threads_Leaderboard_4", size: [[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappingbottom2slot", timeout: site_config.refresh_rate, },
+inreedvid7Slot: { adUnitPath: "/21665095471/SMF_Threads_Leaderboard_4", size: [[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappinginreedvidslot", timeout: site_config.refresh_rate, },
 inreedvid10Slot: { adUnitPath: "/21665095471/SMF_Footer_1", size: [[970, 250],[970, 90],[728, 90],[300, 250],[250, 250],[468, 60],[320, 50],[336, 280],[580, 400],[320, 100]], sizeMapping: "mappingbottom2slot", timeout: site_config.refresh_rate, },
 inreedvid6Slot: { adUnitPath: "/21665095471/SMF_Top_Leaderboard_1", size: [[970, 90],[728, 90],[468, 60],[320, 50],[320, 100]], sizeMapping: "mappingfooterslot", timeout: site_config.refresh_rate, },
 },
@@ -331,3 +331,8 @@ function requestHeaderBidsRefresh(slot) {
 window.setTimeout(function() {
     sendAdserverRequest();
 }, PREBID_TIMEOUT);
+
+
+
+
+
