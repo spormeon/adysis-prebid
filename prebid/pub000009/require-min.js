8c56152e7018fc6bcd0bac96e3a77ea879d1990c
david stanton <davidstanton@spormeon.com>
2020-05-19 16:16:40 +0100

    no message

--- prebid/pub000009/require-min.js
+++ prebid/pub000009/require-min.js
@@ -240,20 +240,8 @@ var site_config = {
 				CONSTANT_BIDDER_STATUS
 				
 				
-				//ASSERTIVE ANALYTICS SETTINGS - Version: 1.4.0
-				var assertive_entityId = "zRMyGjnGD5GTpE6Ra";
-				var assertive_debug = 0; // append the query string 'assertiveYield=debug' or add this local storage entry 'localStorage.setItem("assertiveYield", "debug")' to enable dynamically
-				var assertive_sampleRate = 1; // 1 = 100%, 0.2 = 20%...
-				var assertive_timeout = null;
-				var assertive_layout = null;
-				var assertive_userState = null;
-				//var assertive_custom_1 = null;
-				var assertive_custom_2 = null;
-				var assertive_custom_3 = null;
-				var assertive_custom_4 = null;
-				var assertive_custom_5 = null;
-				//example of supplying a custom var from a prev. defined var
-				assertive_timeout = PREBID_TIMEOUT; 
+				var assertive_entityId="zRMyGjnGD5GTpE6Ra";var assertive_debug=0;var assertive_sampleRate=1;var assertive_timeout=null;var assertive_layout=null;var assertive_userState=null;var assertive_custom_2=null;var assertive_custom_3=null;var assertive_custom_4=null;var assertive_custom_5=null;assertive_timeout=PREBID_TIMEOUT
+
 
 				// NATIVE BIDDER LIST
 				var native_bidders = {
