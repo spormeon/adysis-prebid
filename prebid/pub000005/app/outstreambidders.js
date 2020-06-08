aeacef82b6edb7e83e5a7483f5d584dd5e9da740
david stanton <davidstanton@spormeon.com>
2020-06-08 18:03:22 +0100

    no message

--- prebid/pub000005/app/outstreambidders.js
+++ prebid/pub000005/app/outstreambidders.js
@@ -1,13 +1,9 @@
 //OUTSTREAM BIDDER LIST
 var outstream_bidders = {
 bids: [
-//{ bidder: 'appnexus', params: { placementId: '12842098', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_off'] } } }, /* one placementId for all sizes */
+{ bidder: 'appnexus', params: { placementId: '12842098', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_off'] } } }, /* one placementId for all sizes */
 { bidder: 'districtm', params: { placementId: '15930018', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_off'] } } }, /* one placementId for all sizes */
 { bidder: 'gourmetads', params: { placementId: '19372829', allowSmallerSizes: true, video: { skippable: true, playback_method: ['auto_play_sound_off'] } } } /* one placementId for all sizes */
 //{ bidder: 'conversant', params: { site_id: '117455', mimes: [ 'video/mp4', 'application/javascript' ], maxduration: 60 } }  /* 300x250 */ 
 ] //./bids
 };
-
-
-
-
