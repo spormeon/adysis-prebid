6d37c00c88312d76c88225ffc1dd6fd73f287cbf
david stanton <davidstanton@spormeon.com>
2020-06-10 13:04:43 +0100

    no message

--- prebid/pub000008/app/bannerbidders.js
+++ prebid/pub000008/app/bannerbidders.js
@@ -1,31 +1,38 @@
 // BANNER BIDDER LIST
 var banner_bidders = {
 bids: [
-{ bidder: 'appnexus', params: { placementId: '12995676', allowSmallerSizes: true } }, /* one placementId for all sizes */
-{ bidder: 'districtm', params: { placementId: '19221299', allowSmallerSizes: true } }, /* one placementId for all sizes */
-{ bidder: 'gourmetads', params: { placementId: '12962328', allowSmallerSizes: true } },
-{ bidder: 'districtmDMX', params: { dmxid:'524026', memberid:'101399' } },
-{ bidder: 'conversant', params: { site_id:'117455', secure:1 } },
-{ bidder: 'sovrn', params: { tagid: '590714' } }, /* 728x90 */
-{ bidder: 'sovrn', params: { tagid: '590717' } }, /* 320x50 */
-//{ bidder: 'sovrn', params: { tagid: '725046' } }, /* 336x280 */
-{ bidder: 'sovrn', params: { tagid: '725044' } }, /* 970x90  */
-{ bidder: 'sovrn', params: { tagid: '725085' } }, /* 300x250 */
-//{ bidder: 'sovrn', params: { tagid: '725045' } }, /* 468x60 */
-//{ bidder: 'sovrn', params: { tagid: '725043' } }, /* 970x250 */
-{ bidder: 'openx', params: { unit: '540375630', delDomain: 'huddler-d.openx.net' } }, /* 728x90 970x90 970x250 */
-{ bidder: 'openx', params: { unit: '540375636', delDomain: 'huddler-d.openx.net' } }, /* 320x50 250x250 300x250 */
-{ bidder: 'sharethrough', params: { pkey: "PnWpCKMPrswQY2Qxjvc7DQWC", iframe: true } }, /* Sizeless */
-{ bidder: 'aol', params: { placement: '5219101', network: '11361.1' } }, /* 300x250 */ 
-{ bidder: 'aol', params: { placement: '5219096', network: '11361.1' } }, /* 250x250 */ 
-{ bidder: 'aol', params: { placement: '5219102', network: '11361.1' } }, /* 320x50 */ 
-{ bidder: 'aol', params: { placement: '5219094', network: '11361.1' } }, /* 336x280 */ 
-{ bidder: 'aol', params: { placement: '5219097', network: '11361.1' } }, /* 728x90 */ 
-{ bidder: 'aol', params: { placement: '5219095', network: '11361.1' } }, /* 970x90 */ 
-{ bidder: 'aol', params: { placement: '5219093', network: '11361.1' } }, /* 468x60 */
-{ bidder: 'ix', params: { siteId:'285987', size: [728, 90] } },
-{ bidder: 'ix', params: { siteId:'285987', size: [320, 50] } },
-{ bidder: 'ix', params: { siteId:'285989', size: [300, 250] } }
+{ bidder: 'appnexus', params: { placementId: '13002608', allowSmallerSizes: true } }, /* one placementId for all sizes */
+{ bidder: 'districtm', params: { placementId: '19222008', allowSmallerSizes: true } }, /* one placementId for all sizes */
+//{ bidder: 'gourmetads', params: { placementId: '12962328', allowSmallerSizes: true } },
+{ bidder: 'districtmDMX', params: { dmxid:'524030', memberid:'101399' } },
+//{ bidder: 'conversant', params: { site_id:'117455', secure:1 } },
+{ bidder: 'sovrn', params: { tagid: '736588' } }, /* 728x90 */
+{ bidder: 'sovrn', params: { tagid: '736589' } }, /* 320x50 */
+//{ bidder: 'sovrn', params: { tagid: '736590' } }, /* 336x280 */
+{ bidder: 'sovrn', params: { tagid: '736591' } }, /* 970x90  */
+{ bidder: 'sovrn', params: { tagid: '736583' } }, /* 300x250 */
+//{ bidder: 'sovrn', params: { tagid: '736592' } }, /* 468x60 */
+//{ bidder: 'sovrn', params: { tagid: '736593' } }, /* 970x250 */
+{ bidder: 'openx', params: { unit: '539884776', delDomain: 'huddler-d.openx.net' } }, /* 728x90 */
+{ bidder: 'openx', params: { unit: '539884778', delDomain: 'huddler-d.openx.net' } }, /* 300x250 */
+{ bidder: 'openx', params: { unit: '541128207', delDomain: 'huddler-d.openx.net' } }, /* 320x50 */
+{ bidder: 'openx', params: { unit: '541128208', delDomain: 'huddler-d.openx.net' } }, /* 970x90 */
+{ bidder: 'openx', params: { unit: '541128209', delDomain: 'huddler-d.openx.net' } }, /* 970x250 */
+{ bidder: 'sharethrough', params: { pkey: "MUmdVcQc5gHfJ3CetoAPeP5g", iframe: true } }, /* Sizeless */
+{ bidder: 'aol', params: { placement: '5228967', network: '11361.1' } }, /* 300x250 */ 
+{ bidder: 'aol', params: { placement: '5228972', network: '11361.1' } }, /* 250x250 */ 
+{ bidder: 'aol', params: { placement: '5228975', network: '11361.1' } }, /* 320x50 */ 
+{ bidder: 'aol', params: { placement: '5228966', network: '11361.1' } }, /* 336x280 */ 
+{ bidder: 'aol', params: { placement: '5228971', network: '11361.1' } }, /* 728x90 */ 
+{ bidder: 'aol', params: { placement: '5228973', network: '11361.1' } }, /* 970x90 */ 
+{ bidder: 'aol', params: { placement: '5228974', network: '11361.1' } }, /* 468x60 */
+{ bidder: 'ix', params: { siteId:'285996', size: [728, 90] } },
+{ bidder: 'ix', params: { siteId:'285996', size: [320, 50] } },
+{ bidder: 'ix', params: { siteId:'286001', size: [300, 250] } },
+{ bidder: 'triplelift', params: { inventoryCode: 'niketalk_merc_1' } }, /* 300x250 */
+{ bidder: 'triplelift', params: { inventoryCode: 'niketalk_leaderboard_top' } }, /* 300x250 */
+{ bidder: 'triplelift', params: { inventoryCode: 'niketalk_footer_1' } }, /* 400x225 */
+{ bidder: 'triplelift', params: { inventoryCode: 'niketalk_thread_leaderboard_1' } } /* 400x225 */
 ] //./bids
 };
 
--- prebid/pub000008/app/bannerbidders300x600.js
+++ prebid/pub000008/app/bannerbidders300x600.js
@@ -1,23 +1,24 @@
 // BANNER BIDDER LIST
 var banner_bidders300x600 = {
 bids: [
-{ bidder: 'appnexus', params: { placementId: '12995676', allowSmallerSizes: true } }, /* one placementId for all sizes */
-{ bidder: 'districtm', params: { placementId: '19221299', allowSmallerSizes: true } }, /* one placementId for all sizes */
-{ bidder: 'gourmetads', params: { placementId: '12962328', allowSmallerSizes: true } },
-{ bidder: 'districtmDMX', params: { dmxid:'524026', memberid:'101399' } },
-{ bidder: 'conversant', params: { site_id:'117455', secure:1 } },
-{ bidder: 'sovrn', params: { tagid: '590708' } }, /* 300x600 */
-{ bidder: 'sovrn', params: { tagid: '725085' } }, /* 300x250 */
-{ bidder: 'sovrn', params: { tagid: '725039' } }, /* 160x600 */
-{ bidder: 'sovrn', params: { tagid: '725040' } }, /* 120x600 */
-{ bidder: 'openx', params: { unit: '540375629', delDomain: 'huddler-d.openx.net' } }, /* 300x250 */
-{ bidder: 'openx', params: { unit: '540923378', delDomain: 'huddler-d.openx.net' } }, /* 160x600 300x600 */
-{ bidder: 'sharethrough', params: { pkey: "PnWpCKMPrswQY2Qxjvc7DQWC", iframe: true } }, /* Sizeless */
-{ bidder: 'aol', params: { placement: '5219101', network: '11361.1' } }, /* 300x250 */ 
-{ bidder: 'aol', params: { placement: '5219100', network: '11361.1' } }, /* 120x600 */ 
-{ bidder: 'aol', params: { placement: '5219098', network: '11361.1' } }, /* 160x600 */ 
-{ bidder: 'aol', params: { placement: '5219099', network: '11361.1' } }, /* 300x600 */ 
-{ bidder: 'ix', params: { siteId:'285990', size: [300, 600] } },
-{ bidder: 'ix', params: { siteId:'285989', size: [300, 250] } }
+{ bidder: 'appnexus', params: { placementId: '13002608', allowSmallerSizes: true } }, /* one placementId for all sizes */
+{ bidder: 'districtm', params: { placementId: '19222008', allowSmallerSizes: true } }, /* one placementId for all sizes */
+//{ bidder: 'gourmetads', params: { placementId: '12962328', allowSmallerSizes: true } },
+{ bidder: 'districtmDMX', params: { dmxid:'524030', memberid:'101399' } },
+//{ bidder: 'conversant', params: { site_id:'117455', secure:1 } },
+{ bidder: 'sovrn', params: { tagid: '736582' } }, /* 300x600 */
+{ bidder: 'sovrn', params: { tagid: '736583' } }, /* 300x250 */
+{ bidder: 'sovrn', params: { tagid: '736585' } }, /* 160x600 */
+{ bidder: 'sovrn', params: { tagid: '736586' } }, /* 120x600 */
+{ bidder: 'openx', params: { unit: '539884778', delDomain: 'huddler-d.openx.net' } }, /* 300x250 */
+{ bidder: 'openx', params: { unit: '539884779', delDomain: 'huddler-d.openx.net' } }, /* 160x600 300x600 */
+{ bidder: 'sharethrough', params: { pkey: "MUmdVcQc5gHfJ3CetoAPeP5g", iframe: true } }, /* Sizeless */
+{ bidder: 'aol', params: { placement: '5228967', network: '11361.1' } }, /* 300x250 */ 
+{ bidder: 'aol', params: { placement: '5228970', network: '11361.1' } }, /* 120x600 */ 
+{ bidder: 'aol', params: { placement: '5228968', network: '11361.1' } }, /* 160x600 */ 
+{ bidder: 'aol', params: { placement: '5228969', network: '11361.1' } }, /* 300x600 */ 
+{ bidder: 'ix', params: { siteId:'286002', size: [300, 600] } },
+{ bidder: 'ix', params: { siteId:'286001', size: [300, 250] } },
+{ bidder: 'triplelift', params: { inventoryCode: 'niketalk_skyscraper_1' } } /* 300x600 */
 ] //./bids
 };
