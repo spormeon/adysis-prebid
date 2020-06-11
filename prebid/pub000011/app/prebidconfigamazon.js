f1903156c6f1cb8882cd33b617cafa30e3a9e5e8
david stanton <davidstanton@spormeon.com>
2020-06-11 16:06:26 +0100

    no message

--- prebid/pub000011/app/prebidconfigamazon.js
+++ prebid/pub000011/app/prebidconfigamazon.js
@@ -30,7 +30,7 @@ pbjs.que.push(function() {
     	//rubicon: {singleRequest: true},
     	priceGranularity: customConfigObjectA,
      consentManagement: { gdpr: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*2, allowAuctionWithoutConsent: true }, usp: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*2 } },
-      cache: {url: "https://prebid.adnxs.com/pbc/v1/cache"},
+      //cache: {url: "https://prebid.adnxs.com/pbc/v1/cache"},
       s2sConfig: {
         accountId: 'e31f627f-53a3-4288-9979-482d3c6ffc76',
         enabled: false,
@@ -332,3 +332,6 @@ function requestHeaderBidsRefresh(slot) {
 window.setTimeout(function() {
     sendAdserverRequest();
 }, PREBID_TIMEOUT);
+
+
+
